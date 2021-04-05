94a9c1e1eb41874483d7a671766251bd18b85fa0
GitzJoey <gitzjoey@live.com>
2021-04-05 16:00:23 +0800

    auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('test');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
