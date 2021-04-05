c77aef68ca162fee1842388d65092d53e9be7f5e
GitzJoey <gitzjoey@live.com>
2021-04-05 11:46:42 +0800

    codebase

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.disableNotifications();
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+mix
+    .sass('resources/sass/codebase/main.scss', 'public/css/codebase/codebase.css')
+    .sass('resources/sass/codebase/codebase/themes/corporate.scss', 'public/css/codebase/themes/')
+    .sass('resources/sass/codebase/codebase/themes/earth.scss', 'public/css/codebase/themes/')
+    .sass('resources/sass/codebase/codebase/themes/elegance.scss', 'public/css/codebase/themes/')
+    .sass('resources/sass/codebase/codebase/themes/flat.scss', 'public/css/codebase/themes/')
+    .sass('resources/sass/codebase/codebase/themes/pulse.scss', 'public/css/codebase/themes/')
+    .js('resources/js/codebase/app.js', 'public/js/codebase/codebase.app.js')
+;
