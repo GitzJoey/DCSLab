e46ef10f5bfc1f5bacfbd703983cdb7b322a4960
GitzJoey <gitzjoey@live.com>
2021-04-05 15:04:06 +0800

    codebase

--- resources/views/layouts/codebase/header.blade.php
+++ resources/views/layouts/codebase/header.blade.php
@@ -1,25 +1,14 @@
-<!-- Header -->
 <header id="page-header">
-    <!-- Header Content -->
     <div class="content-header">
-        <!-- Left Section -->
         <div class="content-header-section">
-            <!-- Toggle Sidebar -->
-            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
             <button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="sidebar_toggle">
                 <i class="fa fa-navicon"></i>
             </button>
-            <!-- END Toggle Sidebar -->
 
-            <!-- Open Search Section -->
-            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
             <button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="header_search_on">
                 <i class="fa fa-search"></i>
             </button>
-            <!-- END Open Search Section -->
 
-            <!-- Layout Options (used just for demonstration) -->
-            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
             <div class="btn-group" role="group">
                 <button type="button" class="btn btn-circle btn-dual-secondary" id="page-header-options-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fa fa-wrench"></i>
@@ -83,13 +72,9 @@
                     </div>
                 </div>
             </div>
-            <!-- END Layout Options -->
         </div>
-        <!-- END Left Section -->
 
-        <!-- Right Section -->
         <div class="content-header-section">
-            <!-- User Dropdown -->
             <div class="btn-group" role="group">
                 <button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fa fa-user d-sm-none"></i>
@@ -110,12 +95,9 @@
                     </a>
                     <div class="dropdown-divider"></div>
 
-                    <!-- Toggle Side Overlay -->
-                    <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
                     <a class="dropdown-item" href="javascript:void(0)" data-toggle="layout" data-action="side_overlay_toggle">
                         <i class="si si-wrench mr-5"></i> Settings
                     </a>
-                    <!-- END Side Overlay -->
 
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="javascript:void(0)">
@@ -123,7 +105,6 @@
                     </a>
                 </div>
             </div>
-            <!-- END User Dropdown -->
 
             <!-- Notifications -->
             <div class="btn-group" role="group">
@@ -196,32 +177,22 @@
                     </a>
                 </div>
             </div>
-            <!-- END Notifications -->
 
-            <!-- Toggle Side Overlay -->
-            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
             <button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="side_overlay_toggle">
                 <i class="fa fa-tasks"></i>
             </button>
-            <!-- END Toggle Side Overlay -->
         </div>
-        <!-- END Right Section -->
     </div>
-    <!-- END Header Content -->
 
-    <!-- Header Search -->
     <div id="page-header-search" class="overlay-header">
         <div class="content-header content-header-fullrow">
             <form action="/dashboard" method="POST">
                 @csrf
                 <div class="input-group">
                     <div class="input-group-prepend">
-                        <!-- Close Search Section -->
-                        <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
                         <button type="button" class="btn btn-secondary" data-toggle="layout" data-action="header_search_off">
                             <i class="fa fa-times"></i>
                         </button>
-                        <!-- END Close Search Section -->
                     </div>
                     <input type="text" class="form-control" placeholder="Search or hit ESC.." id="page-header-search-input" name="page-header-search-input">
                     <div class="input-group-append">
@@ -233,10 +204,7 @@
             </form>
         </div>
     </div>
-    <!-- END Header Search -->
 
-    <!-- Header Loader -->
-    <!-- Please check out the Activity page under Elements category to see examples of showing/hiding it -->
     <div id="page-header-loader" class="overlay-header bg-primary">
         <div class="content-header content-header-fullrow text-center">
             <div class="content-header-item">
@@ -244,6 +212,4 @@
             </div>
         </div>
     </div>
-    <!-- END Header Loader -->
 </header>
-<!-- END Header -->
--- resources/views/layouts/codebase/sidebar.blade.php
+++ resources/views/layouts/codebase/sidebar.blade.php
@@ -1,73 +1,41 @@
-<!-- Sidebar -->
-<!--
-    Helper classes
-
-    Adding .sidebar-mini-hide to an element will make it invisible (opacity: 0) when the sidebar is in mini mode
-    Adding .sidebar-mini-show to an element will make it visible (opacity: 1) when the sidebar is in mini mode
-        If you would like to disable the transition, just add the .sidebar-mini-notrans along with one of the previous 2 classes
-
-    Adding .sidebar-mini-hidden to an element will hide it when the sidebar is in mini mode
-    Adding .sidebar-mini-visible to an element will show it only when the sidebar is in mini mode
-        - use .sidebar-mini-visible-b if you would like to be a block when visible (display: block)
--->
 <nav id="sidebar">
-    <!-- Sidebar Content -->
     <div class="sidebar-content">
-        <!-- Side Header -->
         <div class="content-header content-header-fullrow px-15">
-            <!-- Mini Mode -->
             <div class="content-header-section sidebar-mini-visible-b">
-                <!-- Logo -->
                 <span class="content-header-item font-w700 font-size-xl float-left animated fadeIn">
-                                <span class="text-dual-primary-dark">c</span><span class="text-primary">b</span>
-                            </span>
-                <!-- END Logo -->
+                    <span class="text-dual-primary-dark">c</span><span class="text-primary">b</span>
+                </span>
             </div>
-            <!-- END Mini Mode -->
 
-            <!-- Normal Mode -->
             <div class="content-header-section text-center align-parent sidebar-mini-hidden">
-                <!-- Close Sidebar, Visible only on mobile screens -->
-                <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
                 <button type="button" class="btn btn-circle btn-dual-secondary d-lg-none align-v-r" data-toggle="layout" data-action="sidebar_close">
                     <i class="fa fa-times text-danger"></i>
                 </button>
-                <!-- END Close Sidebar -->
 
-                <!-- Logo -->
                 <div class="content-header-item">
                     <a class="link-effect font-w700" href="/">
                         <i class="si si-fire text-primary"></i>
-                        <span class="font-size-xl text-dual-primary-dark">code</span><span class="font-size-xl text-primary">base</span>
+                        <span class="font-size-xl text-dual-primary-dark">DCS</span><span class="font-size-xl text-primary">Lab</span>
                     </a>
                 </div>
-                <!-- END Logo -->
             </div>
-            <!-- END Normal Mode -->
         </div>
-        <!-- END Side Header -->
 
-        <!-- Sidebar Scrolling -->
         <div class="js-sidebar-scroll">
-            <!-- Side User -->
             <div class="content-side content-side-full content-side-user px-10 align-parent">
-                <!-- Visible only in mini mode -->
                 <div class="sidebar-mini-visible-b align-v animated fadeIn">
-                    <img class="img-avatar img-avatar32" src="{{ asset('media/avatars/avatar15.jpg') }}" alt="">
+                    <img class="img-avatar img-avatar32" src="{{ asset('images/def-user.png') }}" alt="">
                 </div>
-                <!-- END Visible only in mini mode -->
 
-                <!-- Visible only in normal mode -->
                 <div class="sidebar-mini-hidden-b text-center">
                     <a class="img-link" href="javascript:void(0)">
-                        <img class="img-avatar" src="{{ asset('media/avatars/avatar15.jpg') }}" alt="">
+                        <img class="img-avatar" src="{{ asset('images/def-user.png') }}" alt="">
                     </a>
                     <ul class="list-inline mt-10">
                         <li class="list-inline-item">
                             <a class="link-effect text-dual-primary-dark font-size-sm font-w600 text-uppercase" href="javascript:void(0)">J. Smith</a>
                         </li>
                         <li class="list-inline-item">
-                            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
                             <a class="link-effect text-dual-primary-dark" data-toggle="layout" data-action="sidebar_style_inverse_toggle" href="javascript:void(0)">
                                 <i class="si si-drop"></i>
                             </a>
@@ -79,22 +47,19 @@
                         </li>
                     </ul>
                 </div>
-                <!-- END Visible only in normal mode -->
             </div>
-            <!-- END Side User -->
 
-            <!-- Side Navigation -->
             <div class="content-side content-side-full">
                 <ul class="nav-main">
                     <li>
-                        <a class="{{ request()->is('dashboard') ? ' active' : '' }}" href="/dashboard">
+                        <a class="active" href="#">
                             <i class="si si-cup"></i><span class="sidebar-mini-hide">Dashboard</span>
                         </a>
                     </li>
                     <li class="nav-main-heading">
-                        <span class="sidebar-mini-visible">VR</span><span class="sidebar-mini-hidden">Various</span>
+                        <span class="sidebar-mini-visible">VR</span><span class="sidebar-mini-hidden">DEV</span>
                     </li>
-                    <li class="{{ request()->is('pages/*') ? ' open' : '' }}">
+                    <li class="">
                         <a class="nav-submenu" data-toggle="nav-submenu" href="#"><i class="si si-bulb"></i><span class="sidebar-mini-hide">Examples</span></a>
                         <ul>
                             <li>
@@ -108,20 +73,8 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="nav-main-heading">
-                        <span class="sidebar-mini-visible">MR</span><span class="sidebar-mini-hidden">More</span>
-                    </li>
-                    <li>
-                        <a href="/">
-                            <i class="si si-globe"></i><span class="sidebar-mini-hide">Landing</span>
-                        </a>
-                    </li>
                 </ul>
             </div>
-            <!-- END Side Navigation -->
         </div>
-        <!-- END Sidebar Scrolling -->
     </div>
-    <!-- Sidebar Content -->
 </nav>
-<!-- END Sidebar -->
