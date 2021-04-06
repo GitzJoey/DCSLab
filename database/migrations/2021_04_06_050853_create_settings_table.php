08c61c59bd999d98f0900ba744d5b358ebc840f5
GitzJoey <gitzjoey@live.com>
2021-04-06 13:22:45 +0800

    laratrust

---
+++ database/migrations/2021_04_06_050853_create_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('type');
+            $table->string('key');
+            $table->string('value');
+            $table->bigInteger('created_by');
+            $table->bigInteger('updated_by');
+            $table->bigInteger('deleted_by');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
