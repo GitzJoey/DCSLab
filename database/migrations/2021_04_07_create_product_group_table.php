4fe9eee88c44dd7a77ed424e9b42b4f73bff276c
HugoYaw22 <80502098+HugoYaw22@users.noreply.github.com>
2021-04-07 18:28:37 +0700

    Create 2021_04_07_create_product_group_table.php

---
+++ database/migrations/2021_04_07_create_product_group_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductGroupsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('product_groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string('code')->nullable();
+            $table->string('name')->nullable();
+            $table->string('note')->nullable();
+            $table->unsignedBigInteger('created_by')->default(0);
+            $table->unsignedBigInteger('updated_by')->default(0);
+            $table->unsignedBigInteger('deleted_by')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('product_groups');
+    }
+}
