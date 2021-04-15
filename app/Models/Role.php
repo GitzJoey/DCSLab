a9209553e0c2b9e9a1009f642d0d35ac02fabd76
GitzJoey <gitzjoey@live.com>
2021-04-15 17:39:54 +0800

    fix factory error

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Laratrust\Models\LaratrustRole;
+use Vinkla\Hashids\Facades\Hashids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends LaratrustRole
 {
+    use HasFactory;
+
     public $guarded = [];
+
+    protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function hId() {
+        return HashIds::encode($this->attributes['id']);
+    }
 }
