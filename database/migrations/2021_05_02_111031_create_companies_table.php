c6f3fdd2ba5d4762cee973bc85300d9934901a3e
HugoYaw22 <80502098+HugoYaw22@users.noreply.github.com>
2021-05-02 18:39:48 +0700

    aa (#30)
    * add readme_id.md
    
    * add 2021_04_20_061711_create_product_groups_table.php
    
    * add groups/index.blade.php
    add brands/index.blade.php
    add units/index.blade.php
    
    * modify web.php
    add Product_GroupsController.php
    add Models/Product_Group.php
    
    * modify sidebar-ext.blade.php
    
    * rename Product_GroupsController.php -> ProductGroupController.php
    rename Product_Group.php -> ProductGroup.php
    
    * Update readme_id.md
    
    * Update readme_id.md
    
    * Update readme_id.md
    
    * Update readme_id.md
    
    * Update readme_id.md
    
    * add migrations/2021_04_23_073021_create_product_brands_table.php
    add migrations/2021_04_23_073056_create_product_units_table.php
    
    * Update readme_id.md
    
    * Add app/Http/Controllers/ProductBrandController.php
    Add app/Http/Controllers/ProductUnitController.php
    Add app/Models/ProductBrand.php
    Add app/Models/ProductUnit.php
    
    * add database/migrations/2021_04_23_140544_create_purchases_supplier.php
    
    * Update readme_id.md
    
    * Update readme_id.md
    
    * Update readme_id.md
    
    * Update readme_id.md
    
    * add ProductGroupService.php
    add ProductBrandService.php
    add ProductGroupServiceImpl.php
    add ProductBrandServiceImpl.php
    
    delete database/migrations/2016_09_09_000001_create_units_table.php
    delete database/migrations/2021_04_25_092039_create_products_table.php
    delete database/migrations/2021_04_25_093004_create_purchase_suppliers_table.php
    
    add database/migrations/2021_04_25_093050_create_finance_cashs_table.php
    add database/migrations/2021_04_23_140544_create_purchases_supplier.php
    add database/migrations/2021_04_25_092039_create_products_table.php
    add database/migrations/2021_04_25_093357_create_sales_customer_groups_table.php
    add database/migrations/2021_04_25_093550_create_sales_customers_table.php
    
    * Update readme_id.md
    
    * asa
    
    * modify database/migrations/2021_04_25_092039_create_products_table.php
    modify database/migrations/2021_04_25_093050_create_finance_cashs_table.php
    modify database/migrations/2021_04_25_093357_create_sales_customer_groups_table.php
    modify database/migrations/2021_04_25_093550_create_sales_customers_table.php
    
    * add database/migrations/2021_04_29_233213_create_branches.php
    
    * delete database/migrations/2016_09_21_100559_create_warehouses_table.php
    delete database/migrations/2021_04_29_233213_create_branches.php
    
    add database/migrations/2021_05_02_111031_create_companies_table.php
    add database/migrations/2021_05_02_111324_create_branches_table.php
    add database/migrations/2021_05_02_111325_create_warehouses_table.php
    
    Co-authored-by: Hartanto119 <80240259+Hartanto119@users.noreply.github.com>
    Co-authored-by: Hartanto119 <hartantosoraya@gmail.com>
    Co-authored-by: GitzJoey <gitzjoey@live.com>

---
+++ database/migrations/2021_05_02_111031_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code');
+            $table->string('name')->nullable();
+
+			$table->integer('is_active')->nullable();
+            $table->unsignedBigInteger('created_by')->default(0);
+            $table->unsignedBigInteger('updated_by')->default(0);
+            $table->unsignedBigInteger('deleted_by')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
