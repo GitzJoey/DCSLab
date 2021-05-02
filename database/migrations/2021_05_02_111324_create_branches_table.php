803dfa0f84ac826bfebe752c8c309f8ca898838d
hugo <hugoyaw22@gmail.com>
2021-05-02 18:32:33 +0700

    delete database/migrations/2016_09_21_100559_create_warehouses_table.php delete database/migrations/2021_04_29_233213_create_branches.php
    add database/migrations/2021_05_02_111031_create_companies_table.php
    add database/migrations/2021_05_02_111324_create_branches_table.php
    add database/migrations/2021_05_02_111325_create_warehouses_table.php

--- database/migrations/2021_05_02_111324_create_branches_table.php
+++ database/migrations/2021_05_02_111324_create_branches_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBranches extends Migration
+class CreateBranchesTable extends Migration
 {
     /**
      * Run the migrations.
