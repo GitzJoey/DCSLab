c66987c66c29404f8242fb577af2b1dc45404374
GitzJoey <gitzjoey@live.com>
2021-09-05 16:36:04 +0800

    cleanup header

--- resources/views/layouts/codebase/header.blade.php
+++ resources/views/layouts/codebase/header.blade.php
@@ -60,66 +60,11 @@
             <div class="btn-group" role="group">
                 <button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-notifications" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fa fa-flag"></i>
-                    <span class="badge badge-primary badge-pill">5</span>
+                    <span class="badge badge-primary badge-pill"></span>
                 </button>
                 <div class="dropdown-menu dropdown-menu-right min-width-300" aria-labelledby="page-header-notifications">
                     <h5 class="h6 text-center py-10 mb-0 border-b text-uppercase">{{ __('dashboard.notifications') }}</h5>
                     <ul class="list-unstyled my-20">
-                        <li>
-                            <a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-                                <div class="ml-5 mr-15">
-                                    <i class="fa fa-fw fa-check text-success"></i>
-                                </div>
-                                <div class="media-body pr-10">
-                                    <p class="mb-0">You’ve upgraded to a VIP account successfully!</p>
-                                    <div class="text-muted font-size-sm font-italic">15 min ago</div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-                                <div class="ml-5 mr-15">
-                                    <i class="fa fa-fw fa-exclamation-triangle text-warning"></i>
-                                </div>
-                                <div class="media-body pr-10">
-                                    <p class="mb-0">Please check your payment info since we can’t validate them!</p>
-                                    <div class="text-muted font-size-sm font-italic">50 min ago</div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-                                <div class="ml-5 mr-15">
-                                    <i class="fa fa-fw fa-times text-danger"></i>
-                                </div>
-                                <div class="media-body pr-10">
-                                    <p class="mb-0">Web server stopped responding and it was automatically restarted!</p>
-                                    <div class="text-muted font-size-sm font-italic">4 hours ago</div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-                                <div class="ml-5 mr-15">
-                                    <i class="fa fa-fw fa-exclamation-triangle text-warning"></i>
-                                </div>
-                                <div class="media-body pr-10">
-                                    <p class="mb-0">Please consider upgrading your plan. You are running out of space.</p>
-                                    <div class="text-muted font-size-sm font-italic">16 hours ago</div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-                                <div class="ml-5 mr-15">
-                                    <i class="fa fa-fw fa-plus text-primary"></i>
-                                </div>
-                                <div class="media-body pr-10">
-                                    <p class="mb-0">New purchases! +$250</p>
-                                    <div class="text-muted font-size-sm font-italic">1 day ago</div>
-                                </div>
-                            </a>
-                        </li>
                     </ul>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item text-center mb-0" href="javascript:void(0)">
