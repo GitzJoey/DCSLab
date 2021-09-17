0091428702aec504fe1431c3b30613a0978d3971
GitzJoey <gitzjoey@live.com>
2021-09-17 12:57:12 +0800

    revert base control

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,15 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\View;
 
 class BaseController extends Controller
 {
     public function __construct()
     {
-        $user = Auth::user();
 
-        View::share('user', $user);
     }
 }
