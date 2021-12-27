072966e59fe3cc6e1fa270fc1fa8786296ea97fc
GitzJoey <gitzjoey@live.com>
2021-12-27 15:27:11 +0800

    initial commit

--- tests/Feature/API/UserAPITest.php
+++ tests/Feature/API/UserAPITest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\API;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
--- tests/Feature/Service/UserServiceTest.php
+++ tests/Feature/Service/UserServiceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Service;
 
 use App\Models\User;
 use App\Services\UserService;
