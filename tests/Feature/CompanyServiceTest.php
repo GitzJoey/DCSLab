460876ab227db226b832d00026730bdd33896dc3
HugoYaw22 <hugoyaw22@gmail.com>
2022-01-02 16:00:42 +0700

    update CompanyServiceTest.php

--- tests/Feature/CompanyServiceTest.php
+++ tests/Feature/CompanyServiceTest.php
@@ -2,21 +2,127 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Company;
+use App\Actions\RandomGenerator;
+use App\Services\CompanyService;
+use Vinkla\Hashids\Facades\Hashids;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CompanyServiceTest extends TestCase
 {
+    use WithFaker;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->service = app(CompanyService::class);
+    }
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_read()
     {
-        $response = $this->get('/');
+        $userId = User::inRandomOrder()->get()[0]->id;
+        $response = $this->service->read($userId);
 
-        $response->assertStatus(200);
+        $this->assertInstanceOf(Paginator::class, $response);
+        $this->assertTrue(!is_null($response));
+    }
+    
+    public function test_create()
+    {
+        $code = (new RandomGenerator())->generateNumber(1, 9999);
+        $name = $this->faker->name;
+        $default = 0;
+        $status = (new RandomGenerator())->generateNumber(0, 1);
+        $userId = User::inRandomOrder()->get()[0]->id;
+
+        $this->service->create(
+            $code,
+            $name,
+            $default,
+            $status,
+            $userId
+        );
+
+        $this->assertDatabaseHas('companies', [
+            'code' => $code,
+            'name' => $name,
+            'status' => $status
+        ]);
+    }
+
+    public function test_update()
+    {
+        $code = (new RandomGenerator())->generateNumber(1, 9999);
+        $name = $this->faker->name;
+        $default = 0;
+        $status = (new RandomGenerator())->generateNumber(0, 1);
+        $userId = User::inRandomOrder()->get()[0]->id;
+
+        $response = $this->service->create(
+            $code,
+            $name,
+            $default,
+            $status,
+            $userId
+        );
+        $rId = Hashids::decode($response)[0];
+
+        $code_new = (new RandomGenerator())->generateNumber(1, 9999);
+        $name_new = $this->faker->name;
+        $default_new = 0;
+        $status_new = (new RandomGenerator())->generateNumber(0, 1);
+        $userId_new = User::inRandomOrder()->get()[0]->id;
+        $response = $this->service->update(
+            $rId,
+            $code_new,
+            $name_new,
+            $default_new,
+            $status_new
+        );
+
+
+        $this->assertDatabaseHas('companies', [
+            'id' => $rId,
+            'code' => $code_new,
+            'name' => $name_new,
+            'default' => $default_new,
+            'status' => $status_new
+        ]);
+    }
+
+    public function test_delete()
+    {
+        $code = (new RandomGenerator())->generateNumber(1, 9999);
+        $name = $this->faker->name;
+        $default = 0;
+        $status = (new RandomGenerator())->generateNumber(0, 1);
+        $userId = User::inRandomOrder()->get()[0]->id;
+
+        $response = $this->service->create(
+            $code,
+            $name,
+            $default,
+            $status,
+            $userId
+        );
+        $rId = Hashids::decode($response)[0];
+
+        $response = $this->service->delete($userId, $rId);
+        $deleted_at = Company::withTrashed()->find($rId)->deleted_at->format('Y-m-d H:i:s');
+        
+        $this->assertDatabaseHas('companies', [
+            'id' => $rId,
+            'deleted_at' => $deleted_at
+        ]);
     }
 }
