08e33498b9d5210630997d2660f147df6078fd8f
GitzJoey <gitzjoey@live.com>
2022-01-14 17:38:28 +0800

    initial commit

--- tests/Unit/RandomGeneratorTest.php
+++ tests/Unit/RandomGeneratorTest.php
@@ -2,16 +2,48 @@
 
 namespace Tests\Unit;
 
+use App\Actions\RandomGenerator;
 use PHPUnit\Framework\TestCase;
 
 class RandomGeneratorTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    protected function setUp(): void
+    {
+        $this->instance = new RandomGenerator();
+    }
+
+    public function test_call_seed()
+    {
+        $this->instance->seed(3);
+        $this->assertTrue(true);
+    }
+
+    public function test_call_generateNumber()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_call_generateFixedLengthNumber()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_call_generateOne()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_call_generateAlphaNumeric()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_call_randomTrueOrFalse()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_call_generateRandomOneZero()
     {
         $this->assertTrue(true);
     }
