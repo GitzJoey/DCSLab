cf4de0e4daad2fbc8fa2c04b0ede278c0378d0ac
GitzJoey <gitzjoey@live.com>
2022-01-28 09:52:26 +0800

    initial commit

---
+++ app/Http/Resources/SupplierResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SupplierResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'code' => $this->code,
+            'name' => $this->name,
+            'contact' => $this->contact,
+            'address' => $this->address,
+            'city' => $this->city,
+            'payment_term_type' => $this->payment_term_type,
+            'payment_term' => $this->payment_term,
+            'taxable_enterprise' => $this->taxable_enterprise,
+            'tax_id' => $this->tax_id,
+            'remarks' => $this->remarks,
+            'status' => $this->status,
+            'selected_products' => '',
+            'main_products' => '',
+            'supplier_poc' => '',
+            'supplier_products' => ''  
+        ];
+    }
+}
