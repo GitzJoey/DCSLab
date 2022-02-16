33d4c64e0ec4b0600939b776625eba658c2d545f
gitzjoey <gitzjoey@live.com>
2022-02-16 23:15:33 +0700

    initial commit

--- tests/Feature/API/BranchAPITest.php
+++ tests/Feature/API/BranchAPITest.php
@@ -1,25 +1,26 @@
 <?php
 
-namespace Tests\Feature\API;
+namespace Tests\Feature;
 
+use App\Models\Company;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\APITestCase;
 
-class UserAPITest extends APITestCase
+class BranchAPITest extends APITestCase
 {
     public function test_api_call_require_authentication()
     {
-        $api = $this->getJson('/api/get/dashboard/admin/users/read');
+        $api = $this->getJson('/api/get/dashboard/company/branch/read');
         $this->assertContains($api->getStatusCode(), array(401, 405));
 
-        $api = $this->getJson('/api/get/dashboard/admin/users/roles/read');
+        $api = $this->getJson('/api/post/dashboard/company/branch/save');
         $this->assertContains($api->getStatusCode(), array(401, 405));
 
-        $api = $this->getJson('/api/post/dashboard/admin/users/save');
+        $api = $this->getJson('/api/post/dashboard/company/branch/edit/1');
         $this->assertContains($api->getStatusCode(), array(401, 405));
 
-        $api = $this->getJson('/api/post/dashboard/admin/users/edit/1');
+        $api = $this->getJson('/api/post/dashboard/company/branch/delete/1');
         $this->assertContains($api->getStatusCode(), array(401, 405));
     }
 
@@ -27,11 +28,23 @@ class UserAPITest extends APITestCase
     {
         $this->actingAs($this->user);
 
-        $api = $this->getJson('/api/get/dashboard/admin/users/read');
+        $companyId = Company::inRandomOrder()->get()[0]->id;;
+        $page = 1;
+        $pageSize = 10;
+        $search = '';
+
+        $api = $this->getJson(route('api.post.db.company.branch.save', [
+            'companyId' => $companyId,
+            'page' => $page,
+            'perPage' => $pageSize,
+            'search' => $search
+        ]));
+
         $api->assertStatus(200);
+        $api->assertSuccessful();
     }
 
-    public function test_api_call_getAllRoles()
+    public function test_api_call_read_getPaymentTermType()
     {
         $this->assertTrue(true);
     }
@@ -46,7 +59,7 @@ class UserAPITest extends APITestCase
         $this->assertTrue(true);
     }
 
-    public function test_api_call_resetPassword()
+    public function test_api_call_delete()
     {
         $this->assertTrue(true);
     }
---
+++ tests/Feature/API/ProductAPITest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\API;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\APITestCase;
+
+class ProductAPITest extends APITestCase
+{
+    public function test_api_call_require_authentication()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_api_call_read()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_api_call_read_getPaymentTermType()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_api_call_store()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_api_call_update()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_api_call_delete()
+    {
+        $this->assertTrue(true);
+    }
+}
