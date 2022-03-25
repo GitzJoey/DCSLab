6e6d97fe50b3243061fc558287f6912ce3e1b912
GitzJoey <gitzjoey@live.com>
2022-03-25 09:21:03 +0800

    initial commit

--- resources/js/midone/ziggy/index.js
+++ resources/js/midone/ziggy/index.js
@@ -2,11 +2,6 @@ import { Ziggy } from "@/ziggy/ziggy";
 import ZiggyJS from "ziggy-js";
 
 function route(name, params) {
-    if (Ziggy.url !== window.location.host) {
-        Ziggy.url = window.location.host;
-        Ziggy.port = window.location.port;
-    }
-    
     return ZiggyJS(name, params, undefined, Ziggy);
 }
 
