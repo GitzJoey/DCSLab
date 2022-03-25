994bf4778dbceb84f8134eaa9613a01ec4e3368b
GitzJoey <gitzjoey@live.com>
2022-03-25 16:46:07 +0800

    initial commit

--- resources/js/midone/router/route-error.js
+++ resources/js/midone/router/route-error.js
@@ -1,5 +1,6 @@
 import SideMenu from "@/layouts/side-menu/Main.vue";
 import Error403 from "@/views/error/403.vue";
+import Error401 from "@/views/error/403.vue";
 
 const root = '/dashboard';
 
@@ -14,7 +15,18 @@ export default {
             component: Error403,
             meta: { 
                 remember: false,
-                log_route: false
+                log_route: false,
+                skipBeforeEach: true
+            }
+        },
+        {
+            path: root + '/error' + '/401',
+            name: 'side-menu-error-401',
+            component: Error401,
+            meta: { 
+                remember: false,
+                log_route: false,
+                skipBeforeEach: true
             }
         }
     ]
---
+++ resources/js/midone/views/error/401.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script setup>
+//#region Imports
+//#endregion
+
+//#region Declarations
+//#endregion
+
+//#region Data - Pinia
+//#endregion
+
+//#region Data - UI
+//#endregion
+
+//#region Data - Views
+//#endregion
+
+//#region onMounted
+//#endregion
+
+//#region Computed
+//#endregion
+
+//#region Watcher
+//#endregion
+</script>
