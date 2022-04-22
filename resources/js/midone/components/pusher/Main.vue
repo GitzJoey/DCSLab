03dfebad2f13001e49cc327c1f716b3284b6e297
GitzJoey <gitzjoey@live.com>
2022-04-22 12:16:11 +0800

    initial commit

--- resources/js/midone/components/pusher/Main.vue
+++ resources/js/midone/components/pusher/Main.vue
@@ -1,22 +1,39 @@
 <template>
+    <Notification refKey="pusherNotification" class="flex flex-col sm:flex-row" :options="{ duration: 3000 }">
+        <div class="ml-4 mr-4">
+            <div class="font-medium">Beep...Beep!</div>
+            <div class="text-slate-500 mt-1">{{ pusherNotificationMessage }}</div>
+        </div>
+    </Notification>
 </template>
 
 <script setup>
 //#region Imports
-import { onMounted, ref } from "vue";
+import { onMounted, ref, provide } from "vue";
 //#endregion
 
 //#region Declarations
+const pusherNotification = ref();
+//#endregion
+
+//#region 
+provide("bind[pusherNotification]", (el) => {
+  pusherNotification.value = el;
+});
+
+provide('triggerPusherNotification', (message) => {
+  pusherNotificationToast(message);
+});
 //#endregion
 
 //#region Data - Pinia
 //#endregion
 
 //#region Data - UI
-const loading = ref(false);
 //#endregion
 
 //#region Data - Views
+const pusherNotificationMessage = ref('');
 //#endregion
 
 //#region onMounted
@@ -27,8 +44,13 @@ onMounted(() => {
 
 //#region Methods
 const listenPusher = () => {
-    Echo.channel('public-channel').listen('.event-pusher', (e) => { console.log(e); });
+    Echo.channel('public-channel').listen('.event-pusher', (e) => { pusherNotificationToast(e); });
 }
+
+const pusherNotificationToast = (message) => {
+    pusherNotificationMessage.value = message;
+    pusherNotification.value.showToast();
+} 
 //#endregion
 
 //#region Computed
