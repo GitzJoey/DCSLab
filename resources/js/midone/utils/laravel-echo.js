759e0135822b908773bd2b887e7b1cbcf2138a1c
GitzJoey <gitzjoey@live.com>
2022-04-26 09:35:15 +0800

    initial commit

--- resources/js/midone/utils/laravel-echo.js
+++ resources/js/midone/utils/laravel-echo.js
@@ -1,27 +1,27 @@
 import Echo from 'laravel-echo';
 
 const install = app => {
-    let pusherEcho = new Echo({
-        broadcaster: 'pusher',
-        key: process.env.MIX_PUSHER_APP_KEY,
-        cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-        forceTLS: true,
-        encryption: true
-    });
-
-    let soketiEcho = new Echo({
-        broadcaster: 'pusher',
-        key: process.env.MIX_SOKETI_APP_KEY,
-        wsHost: process.env.MIX_SOKETI_HOST,
-        wsPort: process.env.MIX_SOKETI_PORT,
-        wssPort: process.env.MIX_SOKETI_PORT,
-        forceTLS: false,
-        encrypted: true,
-        disableStats: true,
-        enabledTransports: ['ws'],
-    });
-
-    window.Echo = process.env.MIX_BROADCAST_DRIVER === 'pusher' ? pusherEcho : soketiEcho;
+    if (process.env.MIX_BROADCAST_DRIVER === 'pusher') {
+        window.Echo = new Echo({
+            broadcaster: 'pusher',
+            key: process.env.MIX_PUSHER_APP_KEY,
+            cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+            forceTLS: true,
+            encryption: true
+        });
+    } else {
+        window.Echo = new Echo({
+            broadcaster: 'pusher',
+            key: process.env.MIX_SOKETI_APP_KEY,
+            wsHost: process.env.MIX_SOKETI_HOST,
+            wsPort: process.env.MIX_SOKETI_PORT,
+            wssPort: process.env.MIX_SOKETI_PORT,
+            forceTLS: false,
+            encrypted: true,
+            disableStats: true,
+            enabledTransports: ['ws'],
+        }); 
+    }
 }
 
 export { install as default }
