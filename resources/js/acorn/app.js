fd4c61b7f1908e877ccfcc28322028f3f1e15df8
GitzJoey <gitzjoey@live.com>
2022-04-27 18:33:28 +0800

    initial commit

--- resources/js/acorn/app.js
+++ resources/js/acorn/app.js
@@ -1,8 +1,2 @@
-window._ = require('lodash');
-
-require('bootstrap');
-
-window.$ = require('cash-dom');
-
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.bootstrap = require('bootstrap');
+window.$ = window.jQuery = require('jquery');
