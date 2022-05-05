11b6880d3bdcd67e962243aa70fca3e5ad59e79a
GitzJoey <gitzjoey@live.com>
2022-05-05 15:31:42 +0800

    initial commit

--- resources/js/midone/global-components/tom-select/index.js
+++ resources/js/midone/global-components/tom-select/index.js
@@ -1,5 +1,6 @@
 import dom from "@left4code/tw-starter/dist/js/dom";
 import TomSelect from "tom-select";
+import _, { clone } from "lodash";
 
 const setValue = (el, props) => {
   if (props.modelValue.length) {
@@ -29,15 +30,72 @@ const init = (originalEl, clonedEl, props, emit, computedOptions) => {
 
   // On change
   clonedEl.TomSelect.on("change", function (selectedItems) {
-    emit("update:modelValue", selectedItems);
+    emit(
+      "update:modelValue",
+      Array.isArray(selectedItems) ? [...selectedItems] : selectedItems
+    );
   });
 };
 
-const reInit = (originalEl, clonedEl, props, emit, computedOptions) => {
-  clonedEl.TomSelect.destroy();
-  dom(clonedEl).html(dom(clonedEl).prev().html());
-  setValue(clonedEl, props);
-  init(originalEl, clonedEl, props, emit, computedOptions);
+const getOptions = (options, tempOptions = []) => {
+  options.each(function (optionKey, optionEl) {
+    if (optionEl instanceof HTMLOptGroupElement) {
+      getOptions(dom(optionEl).children(), tempOptions);
+    } else {
+      tempOptions.push(optionEl);
+    }
+  });
+
+  return tempOptions;
 };
 
-export { setValue, init, reInit };
+const updateValue = (
+  originalEl,
+  clonedEl,
+  modelValue,
+  props,
+  emit,
+  computedOptions
+) => {
+  // Remove old options
+  for (const [optionKey, option] of Object.entries(
+    clonedEl.TomSelect.options
+  )) {
+    if (
+      !getOptions(dom(clonedEl).prev().children()).filter((optionEl) => {
+        return optionEl.value === option.value;
+      }).length
+    ) {
+      clonedEl.TomSelect.removeOption(option.value);
+    }
+  }
+
+  // Add new options
+  dom(clonedEl)
+    .prev()
+    .children()
+    .each(function () {
+      clonedEl.TomSelect.addOption({
+        text: dom(this).text(),
+        value: dom(this).attr("value"),
+      });
+    });
+
+  // Refresh options
+  clonedEl.TomSelect.refreshOptions(false);
+
+  // Update value
+  if (
+    (!Array.isArray(modelValue) &&
+      modelValue !== clonedEl.TomSelect.getValue()) ||
+    (Array.isArray(modelValue) &&
+      !_.isEqual(modelValue, clonedEl.TomSelect.getValue()))
+  ) {
+    clonedEl.TomSelect.destroy();
+    dom(clonedEl).html(dom(clonedEl).prev().html());
+    setValue(clonedEl, props);
+    init(originalEl, clonedEl, props, emit, computedOptions);
+  }
+};
+
+export { setValue, init, updateValue };
