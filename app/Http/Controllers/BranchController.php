7f02de7688b20e4ec111ed2f4231fc7430e58f2f
HugoYaw22 <hugoyaw22@gmail.com>
2022-05-09 23:06:45 +0700

    123123

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -63,11 +63,17 @@ class BranchController extends BaseController
         
         $company_id = Hashids::decode($request['company_id'])[0];
 
-        $code = $request['code'] == config('const.DEFAULT.KEYWORDS.AUTO') ? $code = $this->branchService->generateUniqueCode($company_id) : $request['code'];
-        if (!$this->branchService->isUniqueCode($code, $company_id)) {
-            return response()->error([
-                'code' => trans('rules.unique_code')
-            ]);
+        $code = $request['code'];
+        if ($code == config('const.DEFAULT.KEYWORDS.AUTO')) {
+            do {
+                $code = $this->branchService->generateUniqueCode($company_id);
+            } while (!$this->branchService->isUniqueCode($code, $company_id));
+        } else {
+            if (!$this->branchService->isUniqueCode($code, $company_id)) {
+                return response()->error([
+                    'code' => trans('rules.unique_code')
+                ]);
+            }
         }
 
         $name = $request['name'];
@@ -97,11 +103,17 @@ class BranchController extends BaseController
 
         $company_id = Hashids::decode($request['company_id'])[0];
 
-        $code = $request['code'] == config('const.DEFAULT.KEYWORDS.AUTO') ? $code = $this->branchService->generateUniqueCode($company_id) : $request['code'];
-        if (!$this->branchService->isUniqueCode($code, $company_id, $id)) {
-            return response()->error([
-                'code' => trans('rules.unique_code')
-            ]);
+        $code = $request['code'];
+        if ($code == config('const.DEFAULT.KEYWORDS.AUTO')) {
+            do {
+                $code = $this->branchService->generateUniqueCode($company_id);
+            } while (!$this->branchService->isUniqueCode($code, $company_id, $id));
+        } else {
+            if (!$this->branchService->isUniqueCode($code, $company_id, $id)) {
+                return response()->error([
+                    'code' => trans('rules.unique_code')
+                ]);
+            }
         }
 
         $name = $request['name'];
