bdaf0d5597fc93ba160cf1074da63e4231bf016f
HugoYaw22 <hugoyaw22@gmail.com>
2022-05-14 12:25:27 +0700

    153

--- app/Services/Impls/BranchServiceImpl.php
+++ app/Services/Impls/BranchServiceImpl.php
@@ -116,7 +116,7 @@ class BranchServiceImpl implements BranchService
             return Config::get('const.DEFAULT.ERROR_RETURN_VALUE');
         } finally {
             $execution_time = microtime(true) - $timer_start;
-            Log::channel('perfs')->info('['.session()->getId().'-'.(is_null(auth()->user()) ? '':auth()->id()).'] '.__METHOD__.' ('.number_format($execution_time, 1).'s)');
+            Log::channel('perfs')->info('['.session()->getId().'-'.(is_null(auth()->user()) ? '':auth()->id()).'] '.__METHOD__.' ('.number_format($execution_time, 1).'s)'.($useCache ? ' (C)':' (DB)'));
         }
     }
 
