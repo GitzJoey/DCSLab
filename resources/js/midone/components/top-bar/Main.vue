115c21672991caffec7ba1d2461dca74ea7ff97f
GitzJoey <gitzjoey@live.com>
2022-05-16 11:56:11 +0800

    initial commit

--- resources/js/midone/components/top-bar/Main.vue
+++ resources/js/midone/components/top-bar/Main.vue
@@ -11,11 +11,15 @@
               </DropdownToggle>
               <DropdownMenu class="w-56">
                 <DropdownContent class="dark:bg-dark-6">
-                  <div class="p-2" v-for="(c, cIdx) in userCompanyLists">
-                    <a href="" @click.prevent="switchCompany(c.hId)" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-gray-200 dark:hover:bg-dark-3 rounded-md">
-                      <span :class="{ 'underline': c.name === selectedCompany, 'font-medium': c.default === 1 }">{{ c.name }}</span>
-                    </a>
-                  </div>
+                  <template v-for="(c, cIdx) in userCompanyLists">
+                    <DropdownHeader>{{ c.name }}</DropdownHeader>
+                    <DropdownDivider />
+                    <DropDownItem >
+                      <a href="" @click.prevent="switchCompany(c.hId)" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-gray-200 dark:hover:bg-dark-3 rounded-md">
+                        <span :class="{ 'underline': c.name === selectedCompany, 'font-medium': c.default === 1 }">{{ c.name }}</span>
+                      </a>
+                    </DropDownItem>
+                  </template>
                 </DropdownContent>
               </DropdownMenu>            
           </Dropdown>
@@ -34,16 +38,16 @@
       </DropdownToggle>
       <DropdownMenu class="w-56">
         <DropdownContent tag="div">
-          <div class="p-2">
+          <DropDownItem>
             <a href="" @click.prevent="switchLanguage('en')" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-gray-200 dark:hover:bg-dark-3 rounded-md">
               <img alt="English" :src="assetPath('us.png')" class="w-4 h-4 mr-2" /> <span :class="{ 'font-medium': currentLanguage === 'en' }">English</span>
             </a>
-          </div>
-          <div class="p-2">
+          </DropDownItem>
+          <DropDownItem>
             <a href="" @click.prevent="switchLanguage('id')" class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-gray-200 dark:hover:bg-dark-3 rounded-md">
               <img alt="Bahasa Indonesia" :src="assetPath('id.png')" class="w-4 h-4 mr-2" /> <span :class="{ 'font-medium': currentLanguage === 'id' }">Bahasa Indonesia</span>
-            </a>
-          </div>
+            </a>            
+          </DropDownItem>
         </DropdownContent>
       </DropdownMenu>
     </Dropdown>
