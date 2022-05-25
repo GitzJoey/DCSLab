4b66cda65bde8a50f8c111f4f32da071983f5434
HugoYaw22 <hugoyaw22@gmail.com>
2022-05-25 21:43:43 +0700

    1566

--- app/Http/Requests/BranchRequest.php
+++ app/Http/Requests/BranchRequest.php
@@ -55,6 +55,7 @@ class BranchRequest extends FormRequest
                     'company_id' => ['required', new isValidCompany(), 'bail'],
                     'code' => ['required', 'max:255'],
                     'name' => 'required|max:255',
+                    'is_main' => 'required',
                     'status' => [new Enum(ActiveStatus::class)]
                 ];
                 return array_merge($rules_store, $nullableArr);
@@ -63,6 +64,7 @@ class BranchRequest extends FormRequest
                     'company_id' => ['required', new isValidCompany(), 'bail'],
                     'code' => ['required', 'max:255'],
                     'name' => 'required|max:255',
+                    'is_main' => 'required',
                     'status' => [new Enum(ActiveStatus::class)]
                 ];
                 return array_merge($rules_update, $nullableArr);
