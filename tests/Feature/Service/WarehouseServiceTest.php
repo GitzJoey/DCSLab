0744d73152c8dfae0c1a880b106dcb53b908fee7
DoctorAcc22 <105140626+DoctorAcc22@users.noreply.github.com>
2022-05-28 21:03:40 +0700

    initial commit

--- tests/Feature/Service/WarehouseServiceTest.php
+++ tests/Feature/Service/WarehouseServiceTest.php
@@ -2,17 +2,14 @@
 
 namespace Tests\Feature\Service;
 
-use TypeError;
-use App\Models\Warehouse;
+use App\Models\Branch;
 use App\Models\Company;
+use App\Models\Warehouse;
 use Tests\ServiceTestCase;
-use App\Services\WarehouseService;
 use App\Actions\RandomGenerator;
-use Illuminate\Support\Collection;
-use Vinkla\Hashids\Facades\Hashids;
+use App\Services\WarehouseService;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Contracts\Pagination\Paginator;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class WarehouseServiceTest extends ServiceTestCase
 {
@@ -24,180 +21,265 @@ class WarehouseServiceTest extends ServiceTestCase
 
         $this->service = app(WarehouseService::class);
 
-        if (Warehouse::count() < 2)
+        if (Company::count() < 2) {
+            $this->artisan('db:seed', ['--class' => 'CompanyTableSeeder']);
+        }
+
+        if (Branch::count() < 2) {
+            $this->artisan('db:seed', ['--class' => 'BranchTableSeeder']);
+        }
+
+        if (Warehouse::count() < 2) {
             $this->artisan('db:seed', ['--class' => 'WarehouseTableSeeder']);
+        }
+            
     }
 
-    public function test_call_read_with_empty_search()
+    public function test_call_save_with_all_field_filled()
     {
-        $response = $this->service->read(1,'', true, 10);
-
-        $this->assertInstanceOf(Paginator::class, $response);
-        $this->assertNotNull($response);
-    }
-
-    public function test_call_read_with_special_char_in_search()
-    {
-        $response = $this->service->read(1,'&', true, 10);
-
-        $this->assertNotNull($response);
-        $this->assertInstanceOf(Paginator::class, $response);
-    }
-
-    public function test_call_read_with_negative_value_in_perpage_param()
-    {
-        $response = $this->service->read(-1,'', true, -10);
-
-        $this->assertInstanceOf(Paginator::class, $response);
-        $this->assertNotNull($response);
-    }
-
-    public function test_call_read_without_pagination()
-    {
-        $response = $this->service->read(1,'', false, 10);
-
-        $this->assertInstanceOf(Collection::class, $response);
-    }
-
-    public function test_call_read_with_null_param()
-    {
-        $this->expectException(TypeError::class);
-
-        $this->service->read(null, null, null);
-    }
-
-    public function test_call_create()
-    {
-        $company_id = Company::inRandomOrder()->get()[0]->id;
-        $code = (new RandomGenerator())->generateNumber(1, 9999);
+        $branch_id = Branch::inRandomOrder()->first()->id;
+        $company_id = Branch::where('id', '=', $branch_id)->first()->company_id;
+        $code = (new RandomGenerator())->generateAlphaNumeric(5);
         $name = $this->faker->name;
         $address = $this->faker->address;
         $city = $this->faker->city;
         $contact = $this->faker->e164PhoneNumber;
-        $remarks = null;
+        $remarks = $this->faker->sentence;
         $status = (new RandomGenerator())->generateNumber(0, 1);
 
-        $response = $this->service->create(
-            $company_id,
-            $code,
-            $name,
-            $address,
-            $city,
-            $contact,
-            $remarks,
-            $status
+        $this->service->create(
+            company_id: $company_id,
+            branch_id: $branch_id,
+            code: $code,
+            name: $name,
+            address: $address,
+            city: $city,
+            contact: $contact,
+            remarks: $remarks,
+            status: $status
         );
-
-        $this->assertNotNull($response);
 
         $this->assertDatabaseHas('warehouses', [
             'company_id' => $company_id,
+            'branch_id' => $branch_id,
+            'code' => $code,
+            'name' => $name
+        ]);
+    }
+
+    public function test_call_save_with_minimal_field_filled()
+    {
+        $branch_id = Branch::inRandomOrder()->first()->id;
+        $company_id = Branch::where('id', '=', $branch_id)->first()->company_id;
+        $code = (new RandomGenerator())->generateAlphaNumeric(5);
+        $name = $this->faker->name;
+        $address = null;
+        $city = null;
+        $contact = null;
+        $remarks = null;
+        $status = (new RandomGenerator())->generateNumber(0, 1);
+
+        $this->service->create(
+            company_id: $company_id,
+            branch_id: $branch_id,
+            code: $code,
+            name: $name,
+            address: $address,
+            city: $city,
+            contact: $contact,
+            remarks: $remarks,
+            status: $status
+        );
+
+        $this->assertDatabaseHas('warehouses', [
+            'company_id' => $company_id,
+            'branch_id' => $branch_id,
+            'code' => $code,
+            'name' => $name
+        ]);
+    }
+
+    public function test_call_edit_with_all_field_filled()
+    {
+        $branch_id = Branch::inRandomOrder()->first()->id;
+        $company_id = Branch::where('id', '=', $branch_id)->first()->company_id;
+        $code = (new RandomGenerator())->generateAlphaNumeric(5);
+        $name = $this->faker->name;
+        $address = $this->faker->address;
+        $city = $this->faker->city;
+        $contact = $this->faker->e164PhoneNumber;
+        $remarks = $this->faker->sentence;
+        $status = (new RandomGenerator())->generateNumber(0, 1);
+
+        $warehouse = Warehouse::create([
+            'company_id' => $company_id,
+            'branch_id' => $branch_id,
             'code' => $code,
             'name' => $name,
             'address' => $address,
             'city' => $city,
             'contact' => $contact,
             'remarks' => $remarks,
-            'status' => $status,
+            'status' => $status
+        ]);
+        $id = $warehouse->id;
+
+        $newCode = (new RandomGenerator())->generateAlphaNumeric(5);
+        $newName = $this->faker->name;
+        $newAddress = $this->faker->address;
+        $newCity = $this->faker->city;
+        $newContact = $this->faker->e164PhoneNumber;
+        $newRemarks = $this->faker->sentence;
+        $newStatus = (new RandomGenerator())->generateNumber(0, 1);
+
+        $this->service->update(
+            id: $id,
+            company_id: $company_id,
+            branch_id: $branch_id,
+            code: $newCode,
+            name: $newName,
+            address: $newAddress,
+            city: $newCity,
+            contact: $newContact,
+            remarks: $newRemarks,
+            status: $newStatus
+        );
+
+        $this->assertDatabaseHas('warehouses', [
+            'id' => $id,
+            'company_id' => $company_id,
+            'branch_id' => $branch_id,
+            'code' => $newCode,
+            'name' => $newName
         ]);
     }
 
-    public function test_call_update()
+    public function test_call_edit_with_minimal_field_filled()
     {
-        $company_id = Company::inRandomOrder()->get()[0]->id;
-        $code = (new RandomGenerator())->generateNumber(1, 9999);
+        $branch_id = Branch::inRandomOrder()->first()->id;
+        $company_id = Branch::where('id', '=', $branch_id)->first()->company_id;
+        $code = (new RandomGenerator())->generateAlphaNumeric(5);
         $name = $this->faker->name;
-        $address = $this->faker->address;
-        $city = $this->faker->city;
-        $contact = $this->faker->e164PhoneNumber;
+        $address = null;
+        $city = null;
+        $contact = null;
         $remarks = null;
-        $status = 1;
+        $status = (new RandomGenerator())->generateNumber(0, 1);
 
-        $response = $this->service->create(
-            $company_id,
-            $code,
-            $name,
-            $address,
-            $city,
-            $contact,
-            $remarks,
-            $status
-        );
-
-        $this->assertNotNull($response);
-
-        $code_new = (new RandomGenerator())->generateNumber(1, 9999);
-        $name_new = $this->faker->name;
-        $address_new = $this->faker->address;
-        $city_new = $this->faker->city;
-        $contact_new = $this->faker->e164PhoneNumber;
-        $remarks_new = null;
-        $status_new = (new RandomGenerator())->generateNumber(0, 1);
-
-        $response_edit = $this->service->update(
-            id: $response->id,
-            company_id: $company_id,
-            code: $code_new,
-            name: $name_new,
-            address: $address_new,
-            city: $city_new,
-            contact: $contact_new,
-            remarks: $remarks_new,
-            status: $status_new
-        );
-
-        $this->assertNotNull($response_edit);
-        
-        $this->assertDatabaseHas('warehouses', [
-            'id' => $response_edit->id,
+        $warehouse = Warehouse::create([
             'company_id' => $company_id,
-            'code' => $code_new,
-            'name' => $name_new,
-            'address' => $address_new,
-            'city' => $city_new,
-            'contact' => $contact_new,
-            'remarks' => $remarks_new,
-            'status' => $status_new
+            'branch_id' => $branch_id,
+            'code' => $code,
+            'name' => $name,
+            'address' => $address,
+            'city' => $city,
+            'contact' => $contact,
+            'remarks' => $remarks,
+            'status' => $status
+        ]);
+        $id = $warehouse->id;
+
+        $newCode = (new RandomGenerator())->generateAlphaNumeric(5);
+        $newName = $this->faker->name;
+        $newAddress = null;
+        $newCity = null;
+        $newContact = null;
+        $newRemarks = null;
+        $newStatus = (new RandomGenerator())->generateNumber(0, 1);
+
+        $this->service->update(
+            id: $id,
+            company_id: $company_id,
+            branch_id: $branch_id,
+            code: $newCode,
+            name: $newName,
+            address: $newAddress,
+            city: $newCity,
+            contact: $newContact,
+            remarks: $newRemarks,
+            status: $newStatus
+        );
+
+        $this->assertDatabaseHas('warehouses', [
+            'id' => $id,
+            'company_id' => $company_id,
+            'branch_id' => $branch_id,
+            'code' => $newCode,
+            'name' => $newName
         ]);
     }
 
     public function test_call_delete()
     {
-        $company_id = Company::inRandomOrder()->get()[0]->id;
-        $code = (new RandomGenerator())->generateNumber(1, 9999);
+        $branch_id = Branch::inRandomOrder()->first()->id;
+        $company_id = Branch::where('id', '=', $branch_id)->first()->company_id;
+        $code = (new RandomGenerator())->generateAlphaNumeric(5);
         $name = $this->faker->name;
         $address = $this->faker->address;
         $city = $this->faker->city;
         $contact = $this->faker->e164PhoneNumber;
-        $remarks = null;
+        $remarks = $this->faker->sentence;
         $status = (new RandomGenerator())->generateNumber(0, 1);
 
-        $response = $this->service->create(
-            $company_id,
-            $code,
-            $name,
-            $address,
-            $city,
-            $contact,
-            $remarks,
-            $status
-        );
-
-        $id = $response->id;
+        $warehouse = Warehouse::create([
+            'company_id' => $company_id,
+            'branch_id' => $branch_id,
+            'code' => $code,
+            'name' => $name,
+            'address' => $address,
+            'city' => $city,
+            'contact' => $contact,
+            'remarks' => $remarks,
+            'status' => $status
+        ]);
+        $id = $warehouse->id;
 
         $this->service->delete($id);
-        
+
         $this->assertSoftDeleted('warehouses', [
             'id' => $id
         ]);
     }
-    
-    public function test_call_delete_with_nonexistence_id()
-    {
-        $max_int = 2147483647;
-        
-        $response = $this->service->delete($max_int);
 
-        $this->assertFalse($response);
+    public function test_call_read_when_user_have_warehouses_read_with_empty_search()
+    {
+        $warehouseId = Warehouse::inRandomOrder()->first()->id;
+        $companyId = Warehouse::where('id', '=', $warehouseId)->first()->company_id;
+
+        $response = $this->service->read(
+            companyId: $companyId, 
+            search: '', 
+            paginate: true, 
+            page: 1,
+            perPage: 10,
+            useCache: false
+        );
+
+        $this->assertInstanceOf(Paginator::class, $response);
+        $this->assertNotNull($response);
+    }
+
+    public function test_call_read_when_user_have_warehouses_with_special_char_in_search()
+    {
+        $warehouseId = Warehouse::inRandomOrder()->first()->id;
+        $companyId = Warehouse::where('id', '=', $warehouseId)->first()->company_id;
+
+        $search = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        $paginate = true;
+        $page = 1;
+        $perPage = 10;
+        $useCache = false;
+
+        $response = $this->service->read(
+            companyId: $companyId, 
+            search: $search, 
+            paginate: $paginate, 
+            page: $page,
+            perPage: $perPage,
+            useCache: $useCache
+        );
+
+        $this->assertInstanceOf(Paginator::class, $response);
+        $this->assertNotNull($response);
     }
 }
