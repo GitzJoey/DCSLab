c1071776f49f7205446d51c8442f4c6ce43b595e
GitzJoey <gitzjoey@live.com>
2022-05-31 13:37:54 +0800

    initial commit

--- config/database.php
+++ config/database.php
@@ -65,6 +65,28 @@ return [
             ]) + [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false] : [],
         ],
 
+        'dusk' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge').'_dusk',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CERT => env('MYSQL_ATTR_SSL_CERT'),
+                PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY')
+            ]) + [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false] : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
