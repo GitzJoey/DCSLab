7ecc1ab199b4612a09cfbd39ce3ccd28bece951f
GitzJoey <gitzjoey@live.com>
2022-06-10 16:14:17 +0800

    initial commit

---
+++ tests/Browser/DashboardTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class DashboardTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_dashboard_is_showing()
+    {
+        $loggedInUser = $this->developer;
+
+        $this->browse(function (Browser $browser) use($loggedInUser) {
+            $browser->loginAs($loggedInUser, 'web')
+                    ->visit('/dashboard')
+                    ->waitForLocation('/dashboard')
+                    ->assertSee('Main Dashboard');
+        });
+    }
+}
