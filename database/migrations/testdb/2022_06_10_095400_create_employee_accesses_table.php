f0ea4fdbcfeb1fb2a41a995a87b71b6a8595e3f4
HugoYaw22 <hugoyaw22@gmail.com>
2022-06-10 18:33:21 +0700

    546546

--- database/migrations/testdb/2022_06_10_095400_create_employee_accesses_table.php
+++ database/migrations/testdb/2022_06_10_095400_create_employee_accesses_table.php
@@ -15,17 +15,15 @@ class EmployeeAccessesTable extends Migration
     {
         Schema::create('employee_accesses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('company_id')->references('id')->on('companies');
             $table->foreignId('employee_id')->references('id')->on('employees');
             $table->foreignId('branch_id')->references('id')->on('branches');
-
             $table->unsignedBigInteger('created_by')->default(0);
             $table->unsignedBigInteger('updated_by')->default(0);
             $table->unsignedBigInteger('deleted_by')->default(0);
             $table->timestamps();
             $table->softDeletes();
 
-            $table->index(['company_id', 'employee_id', 'branch_id', 'created_at']);
+            $table->index(['employee_id', 'branch_id']);
         });
     }
 
