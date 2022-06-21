2d16363bc10343773dfbe8de006909f00e2645c6
hugo <hugoyaw22@gmail.com>
2022-06-21 10:03:33 +0700

    45656

--- database/migrations/2021_06_01_040004_create_units_table.php
+++ database/migrations/2021_06_01_040004_create_units_table.php
@@ -19,7 +19,7 @@ class CreateUnitsTable extends Migration
             $table->string('code');
             $table->string('name');
             $table->string('description');
-            $table->integer('category')->default(0);;
+            $table->integer('category')->default(0);
             $table->unsignedBigInteger('created_by')->default(0);
             $table->unsignedBigInteger('updated_by')->default(0);
             $table->unsignedBigInteger('deleted_by')->default(0);
