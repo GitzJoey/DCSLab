bb0a2de90c9dc8cae459b8d854bc51dc2a518733
hugo <hugoyaw22@gmail.com>
2022-06-22 06:06:23 +0700

    4546

--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -24,20 +24,12 @@ class EmployeeResource extends JsonResource
             ]),
             $this->mergeWhen($this->relationLoaded('employeeAccesses'), [
                 'employee_accesses' => EmployeeAccessResource::collection($this->whenLoaded('employeeAccesses')),
-                'selected_companies' => $this->getSelectedCompany($this->whenLoaded('employeeAccesses') ? $this->employee_accesses : null),
-                'selected_accesses' => $this->getSelectedAcsesses($this->whenLoaded('employeeAccesses') ? $this->employee_accesses : null)
+                'selected_accesses' => $this->getSelectedAcsesses($this->whenLoaded('employeeAccesses') ? $this->employeeAccesses : null)
             ]),
             'code' => $this->code,
             'join_date' => $this->join_date,
             'status' => $this->status->name
         ];
-    }
-
-    private function getSelectedCompany($employeeAccesses)
-    {
-        if (is_null($employeeAccesses)) return [];
-
-        return $employeeAccesses->count() != 0 ? $employeeAccesses->pluck('branch.company.hId') : [];
     }
 
     private function getSelectedAcsesses($employeeAccesses)
