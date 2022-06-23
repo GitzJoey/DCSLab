3910ea1fc05c3893fe7406a83492af5186437d35
DoctorAcc22 <105140626+DoctorAcc22@users.noreply.github.com>
2022-06-23 12:56:40 +0700

    initial commit

---
+++ app/Policies/BrandPolicy.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Brand;
+use App\Models\User;
+use App\Enums\UserRoles;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class BrandPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(User $user)
+    {
+        if ($user->roles->isEmpty()) return false;
+
+        if ($user->hasRole(UserRoles::DEVELOPER->value)) return true;
+
+        if ($user->hasPermission('brand-read')) return true;
+    }
+
+    public function view(User $user, Brand $brand = null)
+    {
+        return $this->viewAny($user);
+    }
+
+    public function create(User $user)
+    {
+        if ($user->roles->isEmpty()) return false;
+
+        if ($user->hasRole(UserRoles::DEVELOPER->value)) return true;
+
+        if ($user->hasPermission('brand-create')) return true;
+    }
+
+    public function update(User $user, Brand $brand = null)
+    {
+        if ($user->roles->isEmpty()) return false;
+
+        if ($user->hasRole(UserRoles::DEVELOPER->value)) return true;
+    
+        if ($user->hasPermission('brand-update')) return true;
+    }
+
+    public function delete(User $user, Brand $brand = null)
+    {
+        if ($user->roles->isEmpty()) return false;
+
+        if ($user->hasRole(UserRoles::DEVELOPER->value)) return true;
+    
+        if ($user->hasPermission('brand-delete')) return true;
+    }
+
+    public function restore(User $user, Brand $brand)
+    {
+        return false;
+    }
+
+    public function forceDelete(User $user, Brand $brand)
+    {
+        return false;
+    }
+}
