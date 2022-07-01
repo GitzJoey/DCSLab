6c262810f1605741bdcc2a5e1e741749a390d0db
Shift <shift@laravelshift.com>
2022-07-01 01:19:07 +0000

    Rename ENV variables

--- resources/js/midone/utils/laravel-echo.js
+++ resources/js/midone/utils/laravel-echo.js
@@ -1,21 +1,21 @@
 import Echo from 'laravel-echo';
 
 const install = app => {
-    if (process.env.MIX_BROADCAST_DRIVER === 'pusher' && process.env.MIX_PUSHER_APP_KEY !== '') {
+    if (import.meta.env.VITE_BROADCAST_DRIVER === 'pusher' && import.meta.env.VITE_PUSHER_APP_KEY !== '') {
         window.Echo = new Echo({
             broadcaster: 'pusher',
-            key: process.env.MIX_PUSHER_APP_KEY,
-            cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+            key: import.meta.env.VITE_PUSHER_APP_KEY,
+            cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
             forceTLS: true,
             encryption: true
         });
-    } else if (process.env.MIX_BROADCAST_DRIVER === 'soketi' && process.env.MIX_SOKETI_APP_KEY !== '') {
+    } else if (import.meta.env.VITE_BROADCAST_DRIVER === 'soketi' && import.meta.env.VITE_SOKETI_APP_KEY !== '') {
         window.Echo = new Echo({
             broadcaster: 'pusher',
-            key: process.env.MIX_SOKETI_APP_KEY,
-            wsHost: process.env.MIX_SOKETI_HOST,
-            wsPort: process.env.MIX_SOKETI_PORT,
-            wssPort: process.env.MIX_SOKETI_PORT,
+            key: import.meta.env.VITE_SOKETI_APP_KEY,
+            wsHost: import.meta.env.VITE_SOKETI_HOST,
+            wsPort: import.meta.env.VITE_SOKETI_PORT,
+            wssPort: import.meta.env.VITE_SOKETI_PORT,
             forceTLS: false,
             encrypted: true,
             disableStats: true,
