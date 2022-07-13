45ef2e326a6c948a74880bcd41d957167822faae
GitzJoey <gitzjoey@live.com>
2022-07-13 15:39:24 +0800

    initial commit

--- web/src/utils/index.js
+++ web/src/utils/index.js
@@ -2,10 +2,12 @@ import helper from "./helper";
 import lodash from "./lodash";
 import colors from "./colors";
 import pusher from "./pusher";
+import laravel_echo from "./laravel-echo";
 
 export default (app) => {
   app.use(helper);
   app.use(lodash);
   app.use(colors);
   app.use(pusher);
+  app.use(laravel_echo)
 };
