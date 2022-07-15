8a8b402ee06f27d181c78f6aae970ff9108b84ee
GitzJoey <gitzjoey@live.com>
2022-07-15 20:54:17 +0800

    initial commit

--- web/tests/Login.test.js
+++ web/tests/Login.test.js
@@ -1,23 +1,37 @@
+import { createApp } from "vue";
 import { mount } from "@vue/test-utils";
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 
 import i18n from "@/lang";
 import VeeValidate from "@/validation";
 import { setActivePinia, createPinia } from 'pinia'
+import { useDarkModeStore } from "@/stores/dark-mode";
 
 import LoadingIcon from "@/global-components/loading-icon/Main.vue";
+import DarkModeSwitcher from "@/components/dark-mode-switcher/Main.vue";
 
 import Login from "@/views/login/Main.vue";
 
 describe('Login.vue', () => {
+    const app = createApp({});
+
     beforeEach(() => {
-        setActivePinia(createPinia());
+        const pinia = createPinia().use(useDarkModeStore);
+        app.use(pinia);
+        setActivePinia(pinia);
+    });
+
+    it('can pickup the dark mode switcher value from pinia', () => {
+        const darkMode = useDarkModeStore();
+
+        expect(darkMode.value).toBe(false);
     });
 
     it('should load properly', () => {
         const wrapper = mount(Login, {
             global: {
                 components: {
+                    DarkModeSwitcher,
                     LoadingIcon
                 },
                 plugins: [
