f3e0724a270d4219afa034cbf45ca4ea31c00bc3
GitzJoey <gitzjoey@live.com>
2022-07-18 12:57:51 +0800

    initial commit

--- web/tests/Login.test.js
+++ web/tests/Login.test.js
@@ -1,32 +1,17 @@
-import { createApp } from "vue";
 import { mount } from "@vue/test-utils";
 import { beforeEach, describe, expect, it } from 'vitest';
+import { createTestingPinia } from "@pinia/testing";
 
 import i18n from "@/lang";
 import VeeValidate from "@/validation";
-import { setActivePinia, createPinia } from 'pinia'
-import { useDarkModeStore } from "@/stores/dark-mode";
 
 import LoadingIcon from "@/global-components/loading-icon/Main.vue";
 import DarkModeSwitcher from "@/components/dark-mode-switcher/Main.vue";
+import { useDarkModeStore } from "@/stores/dark-mode";
 
 import Login from "@/views/login/Main.vue";
 
 describe('Login.vue', () => {
-    const app = createApp({});
-
-    beforeEach(() => {
-        const pinia = createPinia().use(useDarkModeStore);
-        app.use(pinia);
-        setActivePinia(pinia);
-    });
-
-    it('can pickup the dark mode switcher value from pinia', () => {
-        const darkMode = useDarkModeStore();
-
-        expect(darkMode.value).toBe(false);
-    });
-
     it('should load properly', () => {
         const wrapper = mount(Login, {
             global: {
@@ -36,10 +21,14 @@ describe('Login.vue', () => {
                 },
                 plugins: [
                     i18n,
-                    VeeValidate
+                    VeeValidate,
+                    createTestingPinia()
                 ]
             }
         });
+
+        const mockDarkModeStore = useDarkModeStore();
+        mockDarkModeStore.darkModeValue = false;
 
         expect(wrapper.text()).contains('Login');
     });
