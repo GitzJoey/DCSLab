c8cb60f4050070172cdf9272ddfcb7b71c4efaf1
GitzJoey <gitzjoey@live.com>
2022-07-18 15:18:37 +0800

    initial commit

--- install.js
+++ install.js
@@ -4,11 +4,11 @@ var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { console.log(stdout); }
 
 if (os.type() === 'Linux') {
-    exec("cd api && composer install && cp .env.example .env", puts);
-    exec("cd web && npm install && cp .env.example .env", puts);  
+    exec("cd api && composer install && cp .env.example .env && exit 1", puts);
+    exec("cd web && npm install && cp .env.example .env && exit 1", puts);  
 } else if (os.type() === 'Windows_NT') {
-    exec("cd api && composer install && copy .env.example .env", puts);
-    exec("cd web && npm install && copy .env.example .env", puts); 
+    exec("cd api && composer install && copy .env.example .env && exit 1", puts);
+    exec("cd web && npm install && copy .env.example .env && exit 1", puts); 
 } else {
     throw new Error("Unsupported OS found: " + os.type());
 }
