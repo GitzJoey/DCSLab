d1372e45909c6c0520cfaa744002b2c9cec4fa52
GitzJoey <gitzjoey@live.com>
2022-07-20 14:06:14 +0800

    initial commit

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'aduh1c',
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
