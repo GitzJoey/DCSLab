f4b9906fb2f6ec5d636d1171d9247d06d60a0c11
GitzJoey <gitzjoey@live.com>
2022-07-27 15:09:44 +0800

    initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'aduh1c',
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:4173',
     viewportWidth: 1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
