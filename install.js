0f33ec115f7a8c3241aace666470ccd1608aac0c
GitzJoey <gitzjoey@live.com>
2022-07-27 11:42:30 +0800

    initial commit

--- install.js
+++ install.js
@@ -5,6 +5,9 @@ const { dirname } = require('path');
 
 const rootDir = dirname(require.main.filename);
 
+let cypressOutput = execSync('npm install');
+console.log(cypressOutput);
+
 process.chdir(rootDir + '/api');
 let outputComposer = execSync('composer install');
 console.log(outputComposer.toString());
