75b35c02c134a5f94ff4fa1595a4cae9ede3a8df
GitzJoey <gitzjoey@live.com>
2022-07-28 23:15:56 +0800

    initial commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,19 @@
+describe('Login Test', () => {
+  it('render the login page', () => {
+    cy.visit('http://localhost:3000/')
+  });
+
+  it('focus on email input element', () => {
+    cy.get('#email').focus();
+  });
+
+  it('show error when email and password input elements empty', () => {
+    cy.get('.btn-primary').click();
+  });
+
+  it('can login', () => {
+    cy.get('#email').type('gitzjoey@yahoo.com');
+    cy.get('#password').type('qweasdzxc');
+    cy.get('.btn-primary').click();
+  });
+});
