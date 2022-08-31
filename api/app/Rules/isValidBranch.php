e5ce7309711d03b8b5ec1febdfaa717d5f65ed3b
HartantoSoraya <80240259+HartantoSoraya@users.noreply.github.com>
2022-08-31 12:16:51 +0700

    56465

---
+++ api/app/Rules/isValidBranch.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class isValidBranch implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return auth()->user()->branches->pluck('id')->contains($value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return trans('rules.valid_branch');
+    }
+}
