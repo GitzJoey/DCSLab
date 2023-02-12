c1376adcd81ec4115fe3a71b6d6c735535db33eb
GitzJoey <gitzjoey@live.com>
2023-02-12 20:55:03 +0800

    initial commit

--- web/src/pages/auth/Login.vue
+++ web/src/pages/auth/Login.vue
@@ -6,6 +6,7 @@ import illustrationUrl from "../../assets/images/illustration.svg";
 import { FormInput, FormCheck } from "../../base-components/Form";
 import Button from "../../base-components/Button";
 import { useI18n } from "vue-i18n";
+import LoadingOverlay from "../../base-components/LoadingOverlay";
 
 const { t } = useI18n();
 
@@ -13,115 +14,117 @@ const appName = import.meta.env.VITE_APP_NAME;
 </script>
 
 <template>
-  <div
-    :class="[
-      '-m-3 sm:-mx-8 p-3 sm:px-8 relative h-screen lg:overflow-hidden bg-primary xl:bg-white dark:bg-darkmode-800 xl:dark:bg-darkmode-600',
-      'before:hidden before:xl:block before:content-[\'\'] before:w-[57%] before:-mt-[28%] before:-mb-[16%] before:-ml-[13%] before:absolute before:inset-y-0 before:left-0 before:transform before:rotate-[-4.5deg] before:bg-primary/20 before:rounded-[100%] before:dark:bg-darkmode-400',
-      'after:hidden after:xl:block after:content-[\'\'] after:w-[57%] after:-mt-[20%] after:-mb-[13%] after:-ml-[13%] after:absolute after:inset-y-0 after:left-0 after:transform after:rotate-[-4.5deg] after:bg-primary after:rounded-[100%] after:dark:bg-darkmode-700',
-    ]"
-  >
-    <DarkModeSwitcher />
-    <MainColorSwitcher />
-    <div class="container relative z-10 sm:px-10">
-      <div class="block grid-cols-2 gap-4 xl:grid">
-        <div class="flex-col hidden min-h-screen xl:flex">
-          <a href="" class="flex items-center pt-5 -intro-x">
-            <img
-              alt="DCSLab"
-              class="w-6"
-              :src="logoUrl"
-            />
-            <span class="ml-3 text-lg text-white"> {{ appName }} </span>
-          </a>
-          <div class="my-auto">
-            <img
-              alt="DCSLab"
-              class="w-1/2 -mt-16 -intro-x"
-              :src="illustrationUrl"
-            />
-            <div
-              class="mt-10 text-4xl font-medium leading-tight text-white -intro-x"
-            >
-              <span class="hidden">A few more clicks to</span><br />
-              <span class="hidden">sign in to your account.</span>
-            </div>
-            <div
-              class="mt-5 text-lg text-white -intro-x text-opacity-70 dark:text-slate-400"
-            >
-              <span class="hidden">Manage all your e-commerce accounts in one place</span>
-            </div>
-          </div>
-        </div>
-        <div class="flex h-screen py-5 my-10 xl:h-auto xl:py-0 xl:my-0">
-          <div
-            class="w-full px-5 py-8 mx-auto my-auto bg-white rounded-md shadow-md xl:ml-20 dark:bg-darkmode-600 xl:bg-transparent sm:px-8 xl:p-0 xl:shadow-none sm:w-3/4 lg:w-2/4 xl:w-auto"
-          >
-            <h2
-              class="text-2xl font-bold text-center intro-x xl:text-3xl xl:text-left"
-            >
-              {{ t('views.login.title') }}
-            </h2>
-            <div class="mt-2 text-center intro-x text-slate-400 xl:hidden">
-              A few more clicks to sign in to your account. Manage all your
-              e-commerce accounts in one place
-            </div>
-            <div class="mt-8 intro-x">
-              <FormInput
-                type="text"
-                class="block px-4 py-3 intro-x login__input min-w-full xl:min-w-[350px]"
-                placeholder="Email"
+
+    <div
+      :class="[
+        '-m-3 sm:-mx-8 p-3 sm:px-8 relative h-screen lg:overflow-hidden bg-primary xl:bg-white dark:bg-darkmode-800 xl:dark:bg-darkmode-600',
+        'before:hidden before:xl:block before:content-[\'\'] before:w-[57%] before:-mt-[28%] before:-mb-[16%] before:-ml-[13%] before:absolute before:inset-y-0 before:left-0 before:transform before:rotate-[-4.5deg] before:bg-primary/20 before:rounded-[100%] before:dark:bg-darkmode-400',
+        'after:hidden after:xl:block after:content-[\'\'] after:w-[57%] after:-mt-[20%] after:-mb-[13%] after:-ml-[13%] after:absolute after:inset-y-0 after:left-0 after:transform after:rotate-[-4.5deg] after:bg-primary after:rounded-[100%] after:dark:bg-darkmode-700',
+      ]"
+    >
+      <DarkModeSwitcher />
+      <MainColorSwitcher />
+      <div class="container relative z-10 sm:px-10">
+        <div class="block grid-cols-2 gap-4 xl:grid">
+          <div class="flex-col hidden min-h-screen xl:flex">
+            <a href="" class="flex items-center pt-5 -intro-x">
+              <img
+                alt="DCSLab"
+                class="w-6"
+                :src="logoUrl"
               />
-              <FormInput
-                type="password"
-                class="block px-4 py-3 mt-4 intro-x login__input min-w-full xl:min-w-[350px]"
-                placeholder="Password"
+              <span class="ml-3 text-lg text-white"> {{ appName }} </span>
+            </a>
+            <div class="my-auto">
+              <img
+                alt="DCSLab"
+                class="w-1/2 -mt-16 -intro-x"
+                :src="illustrationUrl"
               />
-            </div>
-            <div
-              class="flex mt-4 text-xs intro-x text-slate-600 dark:text-slate-500 sm:text-sm"
-            >
-              <div class="flex items-center mr-auto">
-                <FormCheck.Input
-                  id="remember-me"
-                  type="checkbox"
-                  class="mr-2 border"
-                />
-                <label class="cursor-pointer select-none" htmlFor="remember-me">
-                  Remember me
-                </label>
+              <div
+                class="mt-10 text-4xl font-medium leading-tight text-white -intro-x"
+              >
+                <span class="hidden">A few more clicks to</span><br />
+                <span class="hidden">sign in to your account.</span>
               </div>
-              <a href="">Forgot Password?</a>
-            </div>
-            <div class="mt-5 text-center intro-x xl:mt-8 xl:text-left">
-              <Button
-                variant="primary"
-                class="w-full px-4 py-3 align-top xl:w-32 xl:mr-3"
+              <div
+                class="mt-5 text-lg text-white -intro-x text-opacity-70 dark:text-slate-400"
               >
-                Login
-              </Button>
-              <Button
-                variant="outline-secondary"
-                class="w-full px-4 py-3 mt-3 align-top xl:w-32 xl:mt-0"
-              >
-                Register
-              </Button>
-            </div>
-            <div
-              class="mt-10 text-center intro-x xl:mt-24 text-slate-600 dark:text-slate-500 xl:text-left"
-            >
-              By signin up, you agree to our
-              <a class="text-primary dark:text-slate-200" href="">
-                Terms and Conditions
-              </a>
-              &
-              <a class="text-primary dark:text-slate-200" href="">
-                Privacy Policy
-              </a>
+                <span class="hidden">Manage all your e-commerce accounts in one place</span>
+              </div>
             </div>
           </div>
+          <div class="flex h-screen py-5 my-10 xl:h-auto xl:py-0 xl:my-0">
+            <div
+              class="w-full px-5 py-8 mx-auto my-auto bg-white rounded-md shadow-md xl:ml-20 dark:bg-darkmode-600 xl:bg-transparent sm:px-8 xl:p-0 xl:shadow-none sm:w-3/4 lg:w-2/4 xl:w-auto"
+            >
+              <LoadingOverlay :visible="true">
+                <h2
+                  class="text-2xl font-bold text-center intro-x xl:text-3xl xl:text-left"
+                >
+                  {{ t('views.login.title') }}
+                </h2>
+                <div class="mt-2 text-center intro-x text-slate-400 xl:hidden">
+                  A few more clicks to sign in to your account. Manage all your
+                  e-commerce accounts in one place
+                </div>
+                <div class="mt-8 intro-x">
+                  <FormInput
+                    type="text"
+                    class="block px-4 py-3 intro-x login__input min-w-full xl:min-w-[350px]"
+                    placeholder="Email"
+                  />
+                  <FormInput
+                    type="password"
+                    class="block px-4 py-3 mt-4 intro-x login__input min-w-full xl:min-w-[350px]"
+                    placeholder="Password"
+                  />
+                </div>
+                <div
+                  class="flex mt-4 text-xs intro-x text-slate-600 dark:text-slate-500 sm:text-sm"
+                >
+                  <div class="flex items-center mr-auto">
+                    <FormCheck.Input
+                      id="remember-me"
+                      type="checkbox"
+                      class="mr-2 border"
+                    />
+                    <label class="cursor-pointer select-none" htmlFor="remember-me">
+                      Remember me
+                    </label>
+                  </div>
+                  <a href="">Forgot Password?</a>
+                </div>
+                <div class="mt-5 text-center intro-x xl:mt-8 xl:text-left">
+                  <Button
+                    variant="primary"
+                    class="w-full px-4 py-3 align-top xl:w-32 xl:mr-3"
+                  >
+                    Login
+                  </Button>
+                  <Button
+                    variant="outline-secondary"
+                    class="w-full px-4 py-3 mt-3 align-top xl:w-32 xl:mt-0"
+                  >
+                    Register
+                  </Button>
+                </div>
+                <div
+                  class="mt-10 text-center intro-x xl:mt-24 text-slate-600 dark:text-slate-500 xl:text-left"
+                >
+                  By signin up, you agree to our
+                  <a class="text-primary dark:text-slate-200" href="">
+                    Terms and Conditions
+                  </a>
+                  &
+                  <a class="text-primary dark:text-slate-200" href="">
+                    Privacy Policy
+                  </a>
+                </div>
+              </LoadingOverlay>
+            </div>              
+          </div>
         </div>
-        <!-- END: Login Form -->
       </div>
     </div>
-  </div>
 </template>
