fdb2b211587b35b22c993dd2c39fd7771010da2d
GitzJoey <gitzjoey@live.com>
2023-02-16 10:18:44 +0800

    initial commit

--- web/src/services/AuthServices.ts
+++ web/src/services/AuthServices.ts
@@ -1,8 +1,20 @@
+import { AxiosRequestConfig } from "axios";
 import { authAxiosInstance } from "../axios";
 
 export default class AuthService {
-    public doLogin(emailText: string, passwordText: string, rememberMeCheck: boolean): boolean {
+    private async axiosCall<T>(config: AxiosRequestConfig) {
+        try {
+            const { data } = await authAxiosInstance.request(config);
+            return [ null, data];
+        } catch (e) {
+            return [ e ];
+        }
+    }
+
+
+    public async doLogin(emailText: string, passwordText: string, rememberMeCheck: boolean): Promise<boolean> {
         let result = false;
+        
         authAxiosInstance.get('/sanctum/csrf-cookie').then(() => {
             authAxiosInstance.post('login', { email: emailText, password: passwordText }).then(response => {
                 result = true;
