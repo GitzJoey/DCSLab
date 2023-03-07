f7208b9af4146cff5ea3f5d9f8614425df63d85c
GitzJoey <gitzjoey@live.com>
2023-03-07 17:07:46 +0800

    initial commit

--- api/database/migrations/2023_03_05_040333_create_company_user_table.php
+++ api/database/migrations/2023_03_05_040333_create_company_user_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('company_user', function (Blueprint $table) {
             $table->id();
-            $table->ulid();
             $table->foreignId('user_id')->references('id')->on('users');
             $table->foreignId('company_id')->references('id')->on('companies');
         });
