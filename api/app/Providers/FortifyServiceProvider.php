16bb4f9dbf583477b3a446edc8afc48cfa0e36d0
GitzJoey <gitzjoey@live.com>
2023-03-13 09:07:08 +0800

    initial commit

--- api/app/Providers/FortifyServiceProvider.php
+++ api/app/Providers/FortifyServiceProvider.php
@@ -6,10 +6,15 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
+use App\Rules\InactiveUser;
+use App\Rules\MustResetPassword;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\ValidationException;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -40,6 +45,23 @@ class FortifyServiceProvider extends ServiceProvider
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->first();
+
+            if ($user && Hash::check($request->password, $user->password)) {
+                $request->validate([
+                    'email' => [new InactiveUser($user)],
+                    'password' => [new MustResetPassword($user)],
+                ]);
+
+                return $user;
+            } else {
+                throw ValidationException::withMessages([
+                    Fortify::email() => trans('auth.failed'),
+                ]);
+            }
         });
     }
 }
