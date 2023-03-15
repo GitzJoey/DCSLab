cd0387673a0c6ccb1d49e8a565053424b668fdc6
GitzJoey <gitzjoey@live.com>
2023-03-15 15:53:19 +0800

    initial commit

---
+++ api/lang/id/rules.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'unique_code' => 'Code sudah pernah dipakai',
+    'valid_dropdown' => 'Nilai tidak valid',
+    'valid_company' => 'Perusahaan tidak valid',
+    'too_many_tokens' => 'Terlalu banyak permintaan token',
+    'must_reset_password' => 'Harap lakukan reset password',
+    'inactive_user' => 'Profil anda tidak aktif',
+    'company' => [
+        'deactivate_default_company' => 'Perusahaan utama tidak boleh dinonaktifkan',
+        'delete_default_company' => 'Perusahaan utama tidak boleh dihapus',
+    ],
+    'branch' => [
+        'delete_main_branch' => 'Cabang utama tidak boleh dihapus',
+    ],
+];
