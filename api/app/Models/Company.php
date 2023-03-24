aa08cfb4d16e00cb68507ce0253e661e44c5be04
HartantoSoraya <80240259+HartantoSoraya@users.noreply.github.com>
2023-03-24 07:06:15 +0700

    asdas

--- api/app/Models/Company.php
+++ api/app/Models/Company.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use App\Traits\BootableModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class Company extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     use BootableModel;
 
     protected $fillable = [
@@ -102,5 +106,35 @@ class Company extends Model
     public function customerAddresses()
     {
         return $this->hasMany(CustomerAddress::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->ulid = Str::ulid();
+
+            $user = Auth::check();
+            if ($user) {
+                $model->created_by = Auth::id();
+                $model->updated_by = Auth::id();
+            }
+        });
+
+        static::updating(function ($model) {
+            $user = Auth::check();
+            if ($user) {
+                $model->updated_by = Auth::id();
+            }
+        });
+
+        static::deleting(function ($model) {
+            $user = Auth::check();
+            if ($user) {
+                $model->deleted_by = Auth::id();
+                $model->save();
+            }
+        });
     }
 }
--- api/app/Models/CustomerAddress.php
+++ api/app/Models/CustomerAddress.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use App\Traits\BootableModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class CustomerAddress extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     use BootableModel;
 
     protected $table = 'customer_addresses';
@@ -31,5 +35,35 @@ class CustomerAddress extends Model
     public function customer()
     {
         return $this->belongsTo(Customer::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->ulid = Str::ulid();
+
+            $user = Auth::check();
+            if ($user) {
+                $model->created_by = Auth::id();
+                $model->updated_by = Auth::id();
+            }
+        });
+
+        static::updating(function ($model) {
+            $user = Auth::check();
+            if ($user) {
+                $model->updated_by = Auth::id();
+            }
+        });
+
+        static::deleting(function ($model) {
+            $user = Auth::check();
+            if ($user) {
+                $model->deleted_by = Auth::id();
+                $model->save();
+            }
+        });
     }
 }
