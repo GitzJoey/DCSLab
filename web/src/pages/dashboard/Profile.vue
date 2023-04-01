0bafb6e2b665898dc3ca4399e9e6140570d605d5
GitzJoey <gitzjoey@live.com>
2023-04-01 20:04:16 +0800

    initial commit

--- web/src/pages/dashboard/Profile.vue
+++ web/src/pages/dashboard/Profile.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 //#region Imports
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import router from "vue-router";
 import { useI18n } from "vue-i18n";
+import {
+  FormInput,
+  FormLabel,
+} from "../../base-components/Form";
+import { useUserContextStore } from "../../stores/user-context";
 //#endregion
 
 //#region Declarations
 const { t } = useI18n();
+const userContextStore = useUserContextStore();
 //#endregion
 
 //#region Data - Pinia
+const userContext = computed(() => userContextStore.getUserContext);
 //#endregion
 
 //#region Data - UI
@@ -37,6 +44,36 @@ onMounted(() => {
     <h2 class="mr-auto text-lg font-medium">{{ t('views.profile.title') }}</h2>
   </div>
   <div class="grid grid-cols-12 gap-6 mt-5">
-    
+    <div class="col-span-12 intro-y p-5 box">
+      <div class="grid grid-cols-12">
+        <div class="col-span-4">{{ t('views.profile.field_groups.personal_information') }}</div>
+        <div class="col-span-8">
+          <div class="pb-4">
+            <FormLabel htmlFor="name">{{ t('views.profile.fields.name') }}</FormLabel>
+            <FormInput
+              id="name"
+              name="name"
+              type="text"
+              class="w-full"
+              v-model="userContext.name"
+              :placeholder="t('views.profile.fields.name')"
+              readonly
+            />
+          </div>
+          <div class="pb-4">
+            <FormLabel htmlFor="email">{{ t('views.profile.fields.email') }}</FormLabel>
+            <FormInput
+              id="email"
+              name="email"
+              type="text"
+              class="w-full"
+              v-model="userContext.email"
+              :placeholder="t('views.profile.fields.email')"
+              readonly
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
