458e20dce53986c2870b9f313090532a3198f347
GitzJoey <gitzjoey@live.com>
2023-04-02 15:19:33 +0800

    initial commit

--- api/app/Http/Resources/ProfileResource.php
+++ api/app/Http/Resources/ProfileResource.php
@@ -25,10 +25,18 @@ class ProfileResource extends JsonResource
     {
         if ($this->type == 'UserProfile') {
             return [
-                'full_name' => $this->first_name.' '.$this->last_name,
+                'full_name' => $this->createFullName($this->first_name, $this->last_name),
+                'first_name' => $this->first_name,
+                'last_name' => $this->last_name,
+                'address' => $this->address,
+                'city' => $this->city,
+                'postal_code' => $this->postal_code,
                 'country' => $this->country,
                 'status' => $this->status->name,
+                'tax_id' => $this->tax_id,
+                'ic_num' => $this->ic_num,
                 'img_path' => $this->img_path,
+                'remarks' => $this->remarks,
             ];
         } else {
             return [
@@ -45,5 +53,14 @@ class ProfileResource extends JsonResource
                 'remarks' => $this->remarks,
             ];
         }
+    }
+
+    private function createFullName(string $firstName, string $lastName)
+    {
+        if (empty($lastName)) {
+            return $firstName;
+        }
+
+        return $firstName.' '.$lastName;
     }
 }
