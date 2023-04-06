80c1dc5b4c20e74ff5810b2d51e487b698e89768
DoctorAcc22 <105140626+DoctorAcc22@users.noreply.github.com>
2023-04-06 23:00:07 +0700

    initial commit

--- api/app/Http/Resources/UserProfileResource.php
+++ api/app/Http/Resources/UserProfileResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Vinkla\Hashids\Facades\Hashids;
 
 class UserProfileResource extends JsonResource
 {
