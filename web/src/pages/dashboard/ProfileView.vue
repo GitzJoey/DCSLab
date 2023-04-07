db0ad8a41f94657fd237fa2c7384876c287f6bd7
GitzJoey <gitzjoey@live.com>
2023-04-07 13:52:59 +0800

    initial commit

--- web/src/pages/dashboard/ProfileView.vue
+++ web/src/pages/dashboard/ProfileView.vue
@@ -5,6 +5,8 @@ import { useI18n } from "vue-i18n";
 import {
   FormInput,
   FormLabel,
+  FormTextarea,
+  FormSelect,
 } from "../../base-components/Form";
 import { useUserContextStore } from "../../stores/user-context";
 //#endregion
@@ -25,9 +27,7 @@ const userContext = computed(() => userContextStore.getUserContext);
 //#endregion
 
 //#region onMounted
-onMounted(() => {
-
-});
+onMounted(() => {});
 //#endregion
 
 //#region Computed
@@ -39,15 +39,21 @@ onMounted(() => {
 
 <template>
   <div class="flex items-center mt-8 intro-y">
-    <h2 class="mr-auto text-lg font-medium">{{ t('views.profile.title') }}</h2>
+    <h2 class="mr-auto text-xl font-medium">{{ t("views.profile.title") }}</h2>
   </div>
   <div class="grid grid-cols-12 gap-6 mt-5">
     <div class="col-span-12 intro-y p-5 box">
       <div class="grid grid-cols-12">
-        <div class="hidden block md:block lg:block md:col-span-4 lg:col-span-4">{{ t('views.profile.field_groups.personal_information') }}</div>
+        <div class="hidden block md:block lg:block md:col-span-4 lg:col-span-4">
+          <span class="text-lg">{{
+            t("views.profile.field_groups.user_profile")
+          }}</span>
+        </div>
         <div class="col-span-12 md:col-span-8 lg:col-span-8">
           <div class="pb-4">
-            <FormLabel htmlFor="name">{{ t('views.profile.fields.name') }}</FormLabel>
+            <FormLabel htmlFor="name">{{
+              t("views.profile.fields.name")
+            }}</FormLabel>
             <FormInput
               id="name"
               v-model="userContext.name"
@@ -59,7 +65,9 @@ onMounted(() => {
             />
           </div>
           <div class="pb-4">
-            <FormLabel htmlFor="email">{{ t('views.profile.fields.email') }}</FormLabel>
+            <FormLabel htmlFor="email">{{
+              t("views.profile.fields.email")
+            }}</FormLabel>
             <FormInput
               id="email"
               v-model="userContext.email"
@@ -70,13 +78,29 @@ onMounted(() => {
               readonly
             />
           </div>
+        </div>
+      </div>
+      <hr class="py-2" />
+      <div class="grid grid-cols-12">
+        <div class="hidden block md:block lg:block md:col-span-4 lg:col-span-4">
+          {{ t("views.profile.field_groups.personal_information") }}
+        </div>
+        <div class="col-span-12 md:col-span-8 lg:col-span-8">
           <div class="pb-4">
-            <VeeField v-slot="{ field }" name="first_name" rules="required" :label="t('views.profile.fields.first_name')">
-              <FormLabel htmlFor="firstName" class="flex flex-col w-full sm:flex-row">
-                {{ t('views.profile.fields.first_name') }}
+            <VeeField
+              v-slot="{ field }"
+              name="first_name"
+              rules="required"
+              :label="t('views.profile.fields.first_name')"
+            >
+              <FormLabel
+                htmlFor="first_name"
+                class="flex flex-col w-full sm:flex-row"
+              >
+                {{ t("views.profile.fields.first_name") }}
               </FormLabel>
               <FormInput
-                id="firstName"
+                id="first_name"
                 v-model="userContext.profile.first_name"
                 name="first_name"
                 type="text"
@@ -87,9 +111,11 @@ onMounted(() => {
             </VeeField>
           </div>
           <div class="pb-4">
-            <FormLabel htmlFor="lastName">{{ t('views.profile.fields.last_name') }}</FormLabel>
+            <FormLabel htmlFor="last_name">{{
+              t("views.profile.fields.last_name")
+            }}</FormLabel>
             <FormInput
-              id="lastName"
+              id="last_name"
               v-model="userContext.profile.last_name"
               name="last_name"
               type="text"
@@ -97,7 +123,109 @@ onMounted(() => {
               :placeholder="t('views.profile.fields.last_name')"
             />
           </div>
+          <div class="pb-4">
+            <FormLabel htmlFor="address">{{
+              t("views.profile.fields.address")
+            }}</FormLabel>
+            <FormTextarea
+              id="address"
+              v-model="userContext.profile.address"
+              name="address"
+              class="w-full"
+              rows="5"
+              :placeholder="t('views.profile.fields.address')"
+            />
+          </div>
+          <div class="flex gap-2">
+            <div class="pb-4 w-full">
+              <FormLabel htmlFor="city">{{
+                t("views.profile.fields.city")
+              }}</FormLabel>
+              <FormInput
+                id="address"
+                v-model="userContext.profile.city"
+                name="city"
+                type="text"
+                class="w-full"
+                :placeholder="t('views.profile.fields.city')"
+              />
+            </div>
+            <div class="pb-4">
+              <FormLabel htmlFor="postal_code">{{
+                t("views.profile.fields.postal_code")
+              }}</FormLabel>
+              <FormInput
+                id="postal_code"
+                v-model="userContext.profile.postal_code"
+                name="postal_code"
+                type="text"
+                class="w-full"
+                :placeholder="t('views.profile.fields.postal_code')"
+              />
+            </div>
+          </div>
+          <div class="pb-4">
+            <FormLabel htmlFor="country">{{
+              t("views.profile.fields.country")
+            }}</FormLabel>
+            <FormSelect
+              id="country"
+              v-model="userContext.profile.country"
+              name="country"
+              class="w-full"
+              :placeholder="t('views.profile.fields.country')"
+            >
+              <option>Singapore</option>
+              <option>Indonesia</option>
+            </FormSelect>
+          </div>
+          <div class="pb-4">
+            <FormLabel htmlFor="tax_id">{{
+              t("views.profile.fields.tax_id")
+            }}</FormLabel>
+            <FormInput
+              id="tax_id"
+              v-model="userContext.profile.tax_id"
+              name="tax_id"
+              type="text"
+              class="w-full"
+              :placeholder="t('views.profile.fields.tax_id')"
+            />
+          </div>
+          <div class="pb-4">
+            <FormLabel htmlFor="ic_num">{{
+              t("views.profile.fields.ic_num")
+            }}</FormLabel>
+            <FormInput
+              id="ic_num"
+              v-model="userContext.profile.ic_num"
+              name="ic_num"
+              type="text"
+              class="w-full"
+              :placeholder="t('views.profile.fields.ic_num')"
+            />
+          </div>
+          <div class="pb-4">
+            <FormLabel htmlFor="remarks">{{
+              t("views.profile.fields.remarks")
+            }}</FormLabel>
+            <FormTextarea
+              id="remarks"
+              v-model="userContext.profile.remarks"
+              name="remarks"
+              class="w-full"
+              rows="5"
+              :placeholder="t('views.profile.fields.remarks')"
+            />
+          </div>
         </div>
+      </div>
+      <hr class="py-2" />
+      <div class="grid grid-cols-12">
+        <div class="hidden block md:block lg:block md:col-span-4 lg:col-span-4">
+          {{ t("views.profile.field_groups.account_settings") }}
+        </div>
+        <div class="col-span-12 md:col-span-8 lg:col-span-8"></div>
       </div>
     </div>
   </div>
