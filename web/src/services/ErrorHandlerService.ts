1489d83c0ee72194a189e0f9d4dd05a26cad4b41
GitzJoey <gitzjoey@live.com>
2023-04-13 17:05:57 +0800

    initial commit

--- web/src/services/ErrorHandlerService.ts
+++ web/src/services/ErrorHandlerService.ts
@@ -2,22 +2,6 @@ import { AxiosError } from "axios";
 import { ServiceResponseType } from "../types/ServiceResponseType";
 
 export default class ErrorHandlerService {
-    public handleLaravelError(data: any, actions: any, forceSetToField?: string): void {
-        if (data.errors !== undefined && Object.keys(data.errors).length > 0) {
-            for (let key in data.errors) {
-                for (let i = 0; i < data.errors[key].length; i++) {
-                    actions.setFieldError(key, data.errors[key][i]);
-                }
-            }
-        } else {
-            if (forceSetToField) {
-                actions.setFieldError(forceSetToField, data.status + ' ' + data.statusText +': ' + data.message);
-            } else {
-                console.error(data.status + ' ' + data.statusText);
-            }
-        }
-    }
-
     public generateErrorServiceResponse(e: AxiosError): ServiceResponseType<null> {
         if (e.response) {
             return {
