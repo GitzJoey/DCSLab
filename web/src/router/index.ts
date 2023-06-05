c2e2351879661d781994a8a916bcd69a91a8c878
GitzJoey <gitzjoey@live.com>
2023-06-05 15:55:07 +0800

    initial commit

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import SideMenu from "../layouts/SideMenu/SideMenu.vue";
-import SimpleMenu from "../layouts/SimpleMenu/SimpleMenu.vue";
-import TopMenu from "../layouts/TopMenu/TopMenu.vue";
 import Page1 from "../pages/Page1.vue";
 import Page2 from "../pages/Page2.vue";
 
@@ -18,38 +16,6 @@ const routes = [
       {
         path: "page-2",
         name: "side-menu-page-2",
-        component: Page2,
-      },
-    ],
-  },
-  {
-    path: "/simple-menu",
-    component: SimpleMenu,
-    children: [
-      {
-        path: "page-1",
-        name: "simple-menu-page-1",
-        component: Page1,
-      },
-      {
-        path: "page-2",
-        name: "simple-menu-page-2",
-        component: Page2,
-      },
-    ],
-  },
-  {
-    path: "/top-menu",
-    component: TopMenu,
-    children: [
-      {
-        path: "page-1",
-        name: "top-menu-page-1",
-        component: Page1,
-      },
-      {
-        path: "page-2",
-        name: "top-menu-page-2",
         component: Page2,
       },
     ],
