c45d12f45e341207f8a2bf08e403ecaf1f41c702
GitzJoey <gitzjoey@live.com>
2023-06-06 11:39:20 +0800

    initial commit

--- web/src/components/TopBar/TopBar.vue
+++ web/src/components/TopBar/TopBar.vue
@@ -29,14 +29,14 @@ const userContextStore = useUserContextStore();
 const userContext = computed(() => userContextStore.getUserContext);
 const selectedUserCompanyName = computed(() => {
   let companyId = userContextStore.getSelectedUserCompany.ulid;
-  
+
   if (!companyId) return '';
 
   return '';
 });
 const selectedUserBranchName = computed(() => {
   let branchId = userContextStore.getSelectedUserBranch.ulid;
-  
+
   if (!branchId) return '';
 
   return '';
@@ -51,12 +51,9 @@ const appName = import.meta.env.VITE_APP_NAME;
 const logout = () => {
   dashboardStore.toggleScreenMaskValue();
 
-  axios.post('/logout').then(response => {
+  axios.post('/logout').then(() => {
     sessionStorage.clear();
     window.location.href = '/';
-  }).catch((e) => {
-  }).finally(() => {
-    dashboardStore.toggleScreenMaskValue();
   });
 }
 
@@ -78,48 +75,34 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div
-    :class="[
-      'h-[70px] md:h-[65px] z-[51] border-b border-white/[0.08] mt-12 md:mt-0 -mx-3 sm:-mx-8 md:-mx-0 px-3 md:border-b-0 relative md:fixed md:inset-x-0 md:top-0 sm:px-8 md:px-10 md:pt-10 md:bg-gradient-to-b md:from-slate-100 md:to-transparent dark:md:from-darkmode-700',
-      props.layout == 'top-menu' && 'dark:md:from-darkmode-800',
-      'before:content-[\'\'] before:absolute before:h-[65px] before:inset-0 before:top-0 before:mx-7 before:bg-primary/30 before:mt-3 before:rounded-xl before:hidden before:md:block before:dark:bg-darkmode-600/30',
-      'after:content-[\'\'] after:absolute after:inset-0 after:h-[65px] after:mx-3 after:bg-primary after:mt-5 after:rounded-xl after:shadow-md after:hidden after:md:block after:dark:bg-darkmode-600',
-    ]"
-  >
+  <div :class="[
+    'h-[70px] md:h-[65px] z-[51] border-b border-white/[0.08] mt-12 md:mt-0 -mx-3 sm:-mx-8 md:-mx-0 px-3 md:border-b-0 relative md:fixed md:inset-x-0 md:top-0 sm:px-8 md:px-10 md:pt-10 md:bg-gradient-to-b md:from-slate-100 md:to-transparent dark:md:from-darkmode-700',
+    props.layout == 'top-menu' && 'dark:md:from-darkmode-800',
+    'before:content-[\'\'] before:absolute before:h-[65px] before:inset-0 before:top-0 before:mx-7 before:bg-primary/30 before:mt-3 before:rounded-xl before:hidden before:md:block before:dark:bg-darkmode-600/30',
+    'after:content-[\'\'] after:absolute after:inset-0 after:h-[65px] after:mx-3 after:bg-primary after:mt-5 after:rounded-xl after:shadow-md after:hidden after:md:block after:dark:bg-darkmode-600',
+  ]">
     <div class="flex items-center h-full">
-      <RouterLink
-        :to="{ name: 'side-menu-dashboard-maindashboard' }"
-        :class="[
-          '-intro-x hidden md:flex',
-          props.layout == 'side-menu' && 'xl:w-[180px]',
-          props.layout == 'simple-menu' && 'xl:w-auto',
-          props.layout == 'top-menu' && 'w-auto',
-        ]"
-      >
-        <img
-          alt="DCSLab"
-          class="w-6"
-          :src="logoUrl"
-        />
-        <span
-          :class="[
-            'ml-3 text-lg text-white',
-            props.layout == 'side-menu' && 'hidden xl:block',
-            props.layout == 'simple-menu' && 'hidden',
-          ]"
-        >
+      <RouterLink :to="{ name: 'side-menu-dashboard-maindashboard' }" :class="[
+        '-intro-x hidden md:flex',
+        props.layout == 'side-menu' && 'xl:w-[180px]',
+        props.layout == 'simple-menu' && 'xl:w-auto',
+        props.layout == 'top-menu' && 'w-auto',
+      ]">
+        <img alt="DCSLab" class="w-6" :src="logoUrl" />
+        <span :class="[
+          'ml-3 text-lg text-white',
+          props.layout == 'side-menu' && 'hidden xl:block',
+          props.layout == 'simple-menu' && 'hidden',
+        ]">
           {{ appName }}
         </span>
       </RouterLink>
 
-      <Breadcrumb
-        light
-        :class="[
-          'h-[45px] md:ml-10 md:border-l border-white/[0.08] dark:border-white/[0.08] mr-auto -intro-x',
-          props.layout != 'top-menu' && 'md:pl-6',
-          props.layout == 'top-menu' && 'md:pl-10',
-        ]"
-      >
+      <Breadcrumb light :class="[
+        'h-[45px] md:ml-10 md:border-l border-white/[0.08] dark:border-white/[0.08] mr-auto -intro-x',
+        props.layout != 'top-menu' && 'md:pl-6',
+        props.layout == 'top-menu' && 'md:pl-10',
+      ]">
         <Breadcrumb.Text>
           <Menu>
             <Menu.Button :as="Button" variant="primary">
@@ -130,23 +113,16 @@ onMounted(async () => {
             </Menu.Items>
           </Menu>
         </Breadcrumb.Text>
-        <Breadcrumb.Text v-if="selectedUserCompanyName != '' || selectedUserBranchName != ''">{{ selectedUserCompanyName }} - {{ selectedUserBranchName }}</Breadcrumb.Text>
+        <Breadcrumb.Text v-if="selectedUserCompanyName != '' || selectedUserBranchName != ''">{{ selectedUserCompanyName
+        }} - {{ selectedUserBranchName }}</Breadcrumb.Text>
       </Breadcrumb>
 
-      <Button
-        as="a"
-        class="mr-2 intro-x sm:mr-4"
-        href="#"
-        variant="primary"
-        @click="(event: MouseEvent) => { event.preventDefault(); toggleSlideover(true); }"
-      >
+      <Button as="a" class="mr-2 intro-x sm:mr-4" href="#" variant="primary"
+        @click="(event: MouseEvent) => { event.preventDefault(); toggleSlideover(true); }">
         <Lucide icon="Archive" />
       </Button>
 
-      <Slideover
-        :open="showSlideover"
-        @close="() => { toggleSlideover(false); }"
-      >
+      <Slideover :open="showSlideover" @close="() => { toggleSlideover(false); }">
         <Slideover.Panel>
           <Slideover.Title class="p-5">
             <h2 class="mr-auto text-base font-medium">
@@ -157,7 +133,10 @@ onMounted(async () => {
             &nbsp;
           </Slideover.Description>
           <Slideover.Footer>
-            <strong>Copyright &copy; {{ (new Date()).getFullYear() }} <a href="https://www.github.com/GitzJoey">GitzJoey</a>&nbsp;&amp;&nbsp;<a href="https://github.com/GitzJoey/DCSLab/graphs/contributors">Contributors</a>.</strong> All rights reserved.<br/> Powered By Coffee &amp; Curiosity.
+            <strong>Copyright &copy; {{ (new Date()).getFullYear() }} <a
+                href="https://www.github.com/GitzJoey">GitzJoey</a>&nbsp;&amp;&nbsp;<a
+                href="https://github.com/GitzJoey/DCSLab/graphs/contributors">Contributors</a>.</strong> All rights
+            reserved.<br /> Powered By Coffee &amp; Curiosity.
           </Slideover.Footer>
         </Slideover.Panel>
       </Slideover>
@@ -165,7 +144,7 @@ onMounted(async () => {
 
       <Menu class="mr-4 intro-x sm:mr-6">
         <Menu.Button :as="Button" variant="primary">
-          <Lucide icon="GlobeIcon" />
+          <Lucide icon="Globe" />
         </Menu.Button>
         <Menu.Items class="w-48 h-24 overflow-y-auto" placement="bottom-end">
           <Menu.Item><span class="text-primary">English</span></Menu.Item>
@@ -174,19 +153,12 @@ onMounted(async () => {
       </Menu>
 
       <Menu>
-        <Menu.Button
-          class="block w-8 h-8 overflow-hidden rounded-full shadow-lg image-fit zoom-in intro-x"
-        >
-          <img
-            alt="DCSLab"
-            :src="defUserUrl"
-            v-if="!loading"
-          />
-          <LoadingIcon icon="puff" v-else />
+        <Menu.Button class="block w-8 h-8 overflow-hidden rounded-full shadow-lg image-fit zoom-in intro-x">
+          <img v-if="!loading" alt="DCSLab" :src="defUserUrl" />
+          <LoadingIcon v-else icon="puff" />
         </Menu.Button>
         <Menu.Items
-          class="w-56 mt-px relative bg-primary/80 before:block before:absolute before:bg-black before:inset-0 before:rounded-md before:z-[-1] text-white"
-        >
+          class="w-56 mt-px relative bg-primary/80 before:block before:absolute before:bg-black before:inset-0 before:rounded-md before:z-[-1] text-white">
           <Menu.Header class="font-normal">
             <div class="font-medium">{{ userContext.profile.full_name }}</div>
             <div class="text-xs text-white/70 mt-0.5 dark:text-slate-500">
