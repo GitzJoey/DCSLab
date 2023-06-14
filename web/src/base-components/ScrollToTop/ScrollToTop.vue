579b6daed89bfa19c39c6f9d9fc15a4210bc006e
GitzJoey <gitzjoey@live.com>
2023-06-14 16:13:47 +0800

    initial commit

--- web/src/base-components/ScrollToTop/ScrollToTop.vue
+++ web/src/base-components/ScrollToTop/ScrollToTop.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import { toRef } from "vue";
 import Lucide from "../../base-components/Lucide";
 
-const props = defineProps({
-  visible: { type: Boolean, default: true },
-});
+interface ScrollToTopProps {
+  visible: boolean,
+}
 
-const visible = toRef(props, 'visible');
+const props = withDefaults(defineProps<ScrollToTopProps>(), {
+  visible: true,
+});
 
 const goTop = () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -14,7 +15,7 @@ const goTop = () => {
 </script>
 
 <template>
-  <div v-if="visible"
+  <div v-if="props.visible"
     class="fixed bottom-0 right-0 z-50 flex items-center justify-center w-auto h-auto mb-5 mr-5 p-2 border rounded-full shadow-md cursor-pointer box"
     @click="goTop">
     <Lucide icon="ArrowUp" class="w-4 h-4" />
