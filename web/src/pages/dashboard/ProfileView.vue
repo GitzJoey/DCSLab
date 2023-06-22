e8518b1cf60d1c264f208c99b173c2f1c6bae516
GitzJoey <gitzjoey@live.com>
2023-06-22 20:55:48 +0800

    initial commit

--- web/src/pages/dashboard/ProfileView.vue
+++ web/src/pages/dashboard/ProfileView.vue
@@ -127,7 +127,8 @@ const onSubmit = async () => {
               <div class="flex gap-2">
                 <div class="pb-4 w-full">
                   <FormLabel html-for="city">
-                    {{ t("views.profile.fields.city") }}</FormLabel>
+                    {{ t("views.profile.fields.city") }}
+                  </FormLabel>
                   <FormInput id="address" v-model="userContext.profile.city" name="city" type="text" class="w-full"
                     :placeholder="t('views.profile.fields.city')" />
                 </div>
