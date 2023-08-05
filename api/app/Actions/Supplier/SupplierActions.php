a32acbbfc4db2874218b9e26e246f883f223ebee
DoctorAcc22 <105140626+DoctorAcc22@users.noreply.github.com>
2023-08-05 10:32:20 +0700

    465

--- api/app/Actions/Supplier/SupplierActions.php
+++ api/app/Actions/Supplier/SupplierActions.php
@@ -126,7 +126,7 @@ class SupplierActions
                     $this->removeCacheByKey($cacheKey);
                 }
             }
-            
+
             $result = null;
 
             if (! $companyId) {
