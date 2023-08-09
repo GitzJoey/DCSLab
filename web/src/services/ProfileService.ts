ccff7e371eef9e9b41c8d4b0b03e11521592482d
Hafis <hafidz.tafsani@bithealth.co.id>
2023-08-10 03:41:31 +0700

    adjust profile service

--- web/src/services/ProfileService.ts
+++ web/src/services/ProfileService.ts
@@ -30,11 +30,12 @@ export default class ProfileService {
         tax_id : number,
         ic_num : number,
         remarks : string
-    }) : Promise<any> {
+    }) : Promise<ServiceResponse<Resource<{}>|null>> {
+        const result : ServiceResponse<Resource<{}>> = {success : false}
         try {
             const url = route('api.post.db.module.profile.update.info', undefined, false, this.ziggyRoute);
             if(!url) return this.errorHandlerService.generateZiggyUrlErrorServiceResponse();
-            const response = await axios.post(url , {
+            const response : AxiosResponse<Resource<{}>> = await axios.post(url , {
                 first_name : payload.first_name,
                 last_name : payload.last_name,
                 address : payload.address,
@@ -46,10 +47,17 @@ export default class ProfileService {
                 remarks : payload.remarks,
             })
 
-            // console.log(response, "<< ini response")
-            return 
-        } catch (error) {
-            
+            result.success = true
+            result.data = response.data
+            return result
+        } catch (e) {
+            if (e instanceof Error && e.message.includes('Ziggy error')) {
+                return this.errorHandlerService.generateZiggyUrlErrorServiceResponse(e.message);
+            } else if (isAxiosError(e)) {
+                return this.errorHandlerService.generateAxiosErrorServiceResponse(e as AxiosError);
+            } else {
+                return result;
+            }
         }
     }
 
