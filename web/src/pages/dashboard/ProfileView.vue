e5f1d36425082d56e1394dcb6bd9b1cb65f198be
GitzJoey <gitzjoey@live.com>
2023-08-10 13:56:24 +0800

    initial commit

--- web/src/pages/dashboard/ProfileView.vue
+++ web/src/pages/dashboard/ProfileView.vue
@@ -108,7 +108,7 @@ async function handleChangeRole(index: number) {
 async function handleUpdateAccountSettings() {
   const payload = {
     first_name: userContext.value.profile.first_name,
-    last_name: userContext.value.profile.last_name ,
+    last_name: userContext.value.profile.last_name,
     address: userContext.value.profile.address,
     city: userContext.value.profile.city,
     postal_code: userContext.value.profile.postal_code,
@@ -143,12 +143,8 @@ const onSubmit = async () => {
 
       <VeeForm id="profileForm" v-slot="{ errors }" @submit="onSubmit">
         <AlertPlaceholder :errors="errors" />
-        <TwoColumnsLayout
-          :cards="cards"
-          :show-side-tab="true"
-          :using-side-tab="true"
-          @handleExpandCard="handleExpandCard"
-        >
+        <TwoColumnsLayout :cards="cards" :show-side-tab="true" :using-side-tab="true"
+          @handleExpandCard="handleExpandCard">
           <template #side-menu-title>
             {{ userContext.name }}
           </template>
@@ -158,29 +154,15 @@ const onSubmit = async () => {
                 <FormLabel html-for="name">
                   {{ t("views.profile.fields.name") }}
                 </FormLabel>
-                <FormInput
-                  id="name"
-                  v-model="userContext.name"
-                  name="name"
-                  type="text"
-                  class="w-full"
-                  :placeholder="t('views.profile.fields.name')"
-                  readonly
-                />
+                <FormInput id="name" v-model="userContext.name" name="name" type="text" class="w-full"
+                  :placeholder="t('views.profile.fields.name')" readonly />
               </div>
               <div class="pb-4">
                 <FormLabel html-for="email">
                   {{ t("views.profile.fields.email") }}
                 </FormLabel>
-                <FormInput
-                  id="email"
-                  v-model="userContext.email"
-                  name="email"
-                  type="text"
-                  class="w-full"
-                  :placeholder="t('views.profile.fields.email')"
-                  readonly
-                />
+                <FormInput id="email" v-model="userContext.email" name="email" type="text" class="w-full"
+                  :placeholder="t('views.profile.fields.email')" readonly />
               </div>
             </div>
           </template>
@@ -188,94 +170,51 @@ const onSubmit = async () => {
           <template #card-items-1>
             <div class="p-5">
               <div class="pb-4">
-                <VeeField
-                  v-slot="{ field }"
-                  name="first_name"
-                  rules="required"
-                  :label="t('views.profile.fields.first_name')"
-                >
-                  <FormLabel
-                    html-for="first_name"
-                    class="flex flex-col w-full sm:flex-row"
-                  >
+                <VeeField v-slot="{ field }" name="first_name" rules="required"
+                  :label="t('views.profile.fields.first_name')">
+                  <FormLabel html-for="first_name" class="flex flex-col w-full sm:flex-row">
                     {{ t("views.profile.fields.first_name") }}
                   </FormLabel>
-                  <FormInput
-                    id="first_name"
-                    v-model="userContext.profile.first_name"
-                    name="first_name"
-                    type="text"
-                    class="w-full"
-                    :placeholder="t('views.profile.fields.first_name')"
-                    v-bind="field"
-                  />
+                  <FormInput id="first_name" v-model="userContext.profile.first_name" name="first_name" type="text"
+                    class="w-full" :placeholder="t('views.profile.fields.first_name')" v-bind="field" />
                 </VeeField>
               </div>
               <div class="pb-4">
                 <FormLabel html-for="last_name">
                   {{ t("views.profile.fields.last_name") }}
                 </FormLabel>
-                <FormInput
-                  id="last_name"
-                  v-model="userContext.profile.last_name"
-                  name="last_name"
-                  type="text"
-                  class="w-full"
-                  :placeholder="t('views.profile.fields.last_name')"
-                />
+                <FormInput id="last_name" v-model="userContext.profile.last_name" name="last_name" type="text"
+                  class="w-full" :placeholder="t('views.profile.fields.last_name')" />
               </div>
               <div class="pb-4">
                 <FormLabel html-for="address">
                   {{ t("views.profile.fields.address") }}
                 </FormLabel>
-                <FormTextarea
-                  id="address"
-                  v-model="userContext.profile.address"
-                  name="address"
-                  class="w-full"
-                  rows="5"
-                  :placeholder="t('views.profile.fields.address')"
-                />
+                <FormTextarea id="address" v-model="userContext.profile.address" name="address" class="w-full" rows="5"
+                  :placeholder="t('views.profile.fields.address')" />
               </div>
               <div class="flex gap-2">
                 <div class="pb-4 w-full">
                   <FormLabel html-for="city">
                     {{ t("views.profile.fields.city") }}
                   </FormLabel>
-                  <FormInput
-                    id="address"
-                    v-model="userContext.profile.city"
-                    name="city"
-                    type="text"
-                    class="w-full"
-                    :placeholder="t('views.profile.fields.city')"
-                  />
+                  <FormInput id="address" v-model="userContext.profile.city" name="city" type="text" class="w-full"
+                    :placeholder="t('views.profile.fields.city')" />
                 </div>
                 <div class="pb-4">
                   <FormLabel html-for="postal_code">
                     {{ t("views.profile.fields.postal_code") }}
                   </FormLabel>
-                  <FormInput
-                    id="postal_code"
-                    v-model="userContext.profile.postal_code"
-                    name="postal_code"
-                    type="text"
-                    class="w-full"
-                    :placeholder="t('views.profile.fields.postal_code')"
-                  />
+                  <FormInput id="postal_code" v-model="userContext.profile.postal_code" name="postal_code" type="text"
+                    class="w-full" :placeholder="t('views.profile.fields.postal_code')" />
                 </div>
               </div>
               <div class="pb-4">
                 <FormLabel html-for="country">
                   {{ t("views.profile.fields.country") }}
                 </FormLabel>
-                <FormSelect
-                  id="country"
-                  v-model="userContext.profile.country"
-                  name="country"
-                  class="w-full"
-                  :placeholder="t('views.profile.fields.country')"
-                >
+                <FormSelect id="country" v-model="userContext.profile.country" name="country" class="w-full"
+                  :placeholder="t('views.profile.fields.country')">
                   <option>Singapore</option>
                   <option>Indonesia</option>
                 </FormSelect>
@@ -284,48 +223,26 @@ const onSubmit = async () => {
                 <FormLabel html-for="tax_id">
                   {{ t("views.profile.fields.tax_id") }}
                 </FormLabel>
-                <FormInput
-                  id="tax_id"
-                  v-model="userContext.profile.tax_id"
-                  name="tax_id"
-                  type="text"
-                  class="w-full"
-                  :placeholder="t('views.profile.fields.tax_id')"
-                />
+                <FormInput id="tax_id" v-model="userContext.profile.tax_id" name="tax_id" type="text" class="w-full"
+                  :placeholder="t('views.profile.fields.tax_id')" />
               </div>
               <div class="pb-4">
                 <FormLabel html-for="ic_num">
                   {{ t("views.profile.fields.ic_num") }}
                 </FormLabel>
-                <FormInput
-                  id="ic_num"
-                  v-model="userContext.profile.ic_num"
-                  name="ic_num"
-                  type="text"
-                  class="w-full"
-                  :placeholder="t('views.profile.fields.ic_num')"
-                />
+                <FormInput id="ic_num" v-model="userContext.profile.ic_num" name="ic_num" type="text" class="w-full"
+                  :placeholder="t('views.profile.fields.ic_num')" />
               </div>
               <div class="pb-4">
                 <FormLabel html-for="remarks">
                   {{ t("views.profile.fields.remarks") }}
                 </FormLabel>
-                <FormTextarea
-                  id="remarks"
-                  v-model="userContext.profile.remarks"
-                  name="remarks"
-                  class="w-full"
-                  rows="5"
-                  :placeholder="t('views.profile.fields.remarks')"
-                />
+                <FormTextarea id="remarks" v-model="userContext.profile.remarks" name="remarks" class="w-full" rows="5"
+                  :placeholder="t('views.profile.fields.remarks')" />
               </div>
 
               <div class="flex gap-4">
-                <Button
-                  variant="primary"
-                  class="w-28 shadow-md"
-                  @click="handleUpdateAccountSettings"
-                >
+                <Button variant="primary" class="w-28 shadow-md" @click="handleUpdateAccountSettings">
                   {{ t("components.buttons.submit") }}
                 </Button>
 
@@ -344,30 +261,16 @@ const onSubmit = async () => {
                     t("views.profile.fields.change_password.current_password")
                   }}
                 </FormLabel>
-                <FormInput
-                  id="currentPassword"
-                  name="currentPassword"
-                  type="password"
-                  class="w-full"
-                  :placeholder="
-                    t('views.profile.fields.change_password.current_password')
-                  "
-                />
+                <FormInput id="currentPassword" name="currentPassword" type="password" class="w-full" :placeholder="t('views.profile.fields.change_password.current_password')
+                    " />
               </div>
 
               <div class="pb-4">
                 <FormLabel html-for="new_password">
                   {{ t("views.profile.fields.change_password.new_password") }}
                 </FormLabel>
-                <FormInput
-                  id="new_password"
-                  name="new_password"
-                  type="password"
-                  class="w-full"
-                  :placeholder="
-                    t('views.profile.fields.change_password.new_password')
-                  "
-                />
+                <FormInput id="new_password" name="new_password" type="password" class="w-full" :placeholder="t('views.profile.fields.change_password.new_password')
+                  " />
               </div>
 
               <div class="pb-4">
@@ -376,15 +279,8 @@ const onSubmit = async () => {
                     t("views.profile.fields.change_password.confirm_password")
                   }}
                 </FormLabel>
-                <FormInput
-                  id="confirm_password"
-                  name="confirm_password"
-                  type="password"
-                  class="w-full"
-                  :placeholder="
-                    t('views.profile.fields.change_password.confirm_password')
-                  "
-                />
+                <FormInput id="confirm_password" name="confirm_password" type="password" class="w-full" :placeholder="t('views.profile.fields.change_password.confirm_password')
+                    " />
               </div>
             </div>
           </template>
@@ -437,26 +333,14 @@ const onSubmit = async () => {
             <div class="p-5">
               <div class="pb-4">
                 <div class="grid grid-cols-3 gap-2 place-items center">
-                  <div
-                    v-for="(item, index) in roles"
-                    :key="index"
-                    class="flex flex-col items-center"
-                  >
-                    <div
-                      @click="handleChangeRole(index)"
-                      class="cursor-pointer flex flex-col items-center justify-center"
-                    >
+                  <div v-for="(item, index) in roles" :key="index" class="flex flex-col items-center">
+                    <div class="cursor-pointer flex flex-col items-center justify-center"
+                      @click="handleChangeRole(index)">
                       <img alt="" :src="item.images" width="100" height="100" />
-                      <div
-                        v-if="item.active"
-                        class="grid grid-cols-1 place-items-center"
-                      >
+                      <div v-if="item.active" class="grid grid-cols-1 place-items-center">
                         <Check class="text-success" />
                       </div>
-                      <button
-                        v-else
-                        class="btn btn-sm btn-secondary hover:btn-primary"
-                      >
+                      <button v-else class="btn btn-sm btn-secondary hover:btn-primary">
                         {{ t("components.buttons.activate") }}
                       </button>
                     </div>
