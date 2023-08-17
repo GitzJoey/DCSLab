dfa57ef950ef21cebe78e8f934aee5a6c6d05e6c
GitzJoey <gitzjoey@live.com>
2023-08-17 18:22:03 +0800

    initial commit

---
+++ api/lang/id/rules.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'unique_code' => 'Code sudah pernah dipakai',
+    'valid_dropdown' => 'Nilai tidak valid',
+    'valid_company' => 'Perusahaan tidak valid',
+    'valid_branch' => 'Cabang tidak valid',
+    'valid_employee' => 'Karyawan tidak valid',
+    'valid_warehouse' => 'Gudang tidak valid',
+    'valid_product_group' => 'Kelompok product tidak valid',
+    'valid_brand' => 'Merk tidak valid',
+    'valid_unit' => 'Satuan tidak valid',
+    'valid_product' => 'Product tidak valid',
+    'valid_product_unit' => 'Satuan pada product tidak valid',
+    'valid_supplier' => 'Supplier tidak valid',
+    'purchase_order' => [
+        'per_unit_discounts' => 'Discount per unit tidak valid',
+        'per_unit_subtotal_discounts' => 'Discount subtotal per unit tidak valid',
+        'global_discounts' => 'Discount global tidak valid',
+    ],
+    'too_many_tokens' => 'Terlalu banyak permintaan token',
+    'must_reset_password' => 'Harap lakukan reset password',
+    'inactive_user' => 'Profil anda tidak aktif',
+    'company' => [
+        'deactivate_default_company' => 'Perusahaan utama tidak boleh dinonaktifkan',
+        'delete_default_company' => 'Perusahaan utama tidak boleh dihapus',
+    ],
+    'branch' => [
+        'delete_main_branch' => 'Cabang utama tidak boleh dihapus',
+    ],
+];
