8df14a25f4fe17e7c8fceb1c70cc2d9d90c6d91c
GitzJoey <gitzjoey@live.com>
2023-08-17 18:07:37 +0800

    initial commit

---
+++ api/config/dcslab.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'API_TOKEN_NAME' => 'api',
+
+    'PAGINATION_LIMIT' => 10,
+
+    'DATA_CACHE' => [
+        'ENABLED' => env('DCSLAB_DATACACHE', true),
+        'CACHE_TIME' => env('DCSLAB_DATACACHE_TIME', 3600),
+    ],
+
+    'PASSWORD_EXPIRY_DAYS' => 365,
+
+    'ERROR_RETURN_VALUE' => null,
+
+    'KEYWORDS' => [
+        'AUTO' => '_AUTO_',
+    ],
+];
