4244a381c141b196a6633d8fa4805839de0c8773
GitzJoey <gitzjoey@live.com>
2023-09-05 22:52:33 +0800

    initial commit

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'precognition' => \Illuminate\Foundation\Http\HandlePrecognitiveRequests::class,
+        'precognition' => \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
     ];
 }
