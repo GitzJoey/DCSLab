3dcf0a2034afbbdb2eb23cccc9e207829bee8625
GitzJoey <gitzjoey@live.com>
2023-10-13 13:59:56 +0700

    initial commit

--- web/src/base-components/AlertPlaceholder/EmailVerificationAlert.vue
+++ web/src/base-components/AlertPlaceholder/EmailVerificationAlert.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import Alert from "../Alert";
+import Lucide from "../Lucide";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
 </script>
 
 <template>
     <div class="mt-8">
-        <Alert variant="secondary" class="mb-2">
-            Please verify your email to complete the registration process. Check your inbox for a verification link.
+        <Alert variant="secondary" class="flex items-center mb-2">
+            {{ t('components.alert-placeholder.email_verification.message') }}
+            <Alert.DismissButton type="button" class="text-white" aria-label="Close">
+                <Lucide icon="X" class="text-black w-4 h-4" />
+            </Alert.DismissButton>
         </Alert>
     </div>
 </template>
